023076fce334fbcdf0f8101e2f1fedc14560e1d9
danjac <danjac2018@gmail.com>
2019-04-20 23:56:00 +0300

    Webpack config update

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+const webpack = require('webpack');
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./assets/scss/main.scss', './assets/js/main.js'],
+  mode: process.env.NODE_ENV,
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve('./assets/dist/'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
+};
