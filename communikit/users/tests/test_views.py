7be80ccc9f354a4fa5235bdb861da4bf20d93f6f
danjac <danjac2018@gmail.com>
2019-04-21 14:56:53 +0300

    Adding user layout

--- communikit/users/tests/test_views.py
+++ communikit/users/tests/test_views.py
@@ -1,12 +1,39 @@
 import pytest
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import Client
 
 pytestmark = pytest.mark.django_db
 
 
+User = get_user_model()
+
+
 class TestUserDetailView:
-    def test_view(self, client: Client, login_user: settings.AUTH_USER_MODEL):
+    def test_get(self, client: Client, login_user: settings.AUTH_USER_MODEL):
         response = client.get("/account/")
         assert response.status_code == 200
+
+
+class TestUserUpdateView:
+    def test_get(self, client: Client, login_user: settings.AUTH_USER_MODEL):
+        response = client.get("/account/~update/")
+        assert response.status_code == 200
+
+    def test_post(self, client: Client, login_user: settings.AUTH_USER_MODEL):
+        response = client.post("/account/~update/", {"name": "New Name"})
+        assert response.url == "/account/"
+        login_user.refresh_from_db()
+        assert login_user.name == "New Name"
+
+
+class TestUserDeleteView:
+    def test_get(self, client: Client, login_user: settings.AUTH_USER_MODEL):
+        response = client.get("/account/~delete/")
+        assert response.status_code == 200
+
+    def test_post(self, client: Client, login_user: settings.AUTH_USER_MODEL):
+        response = client.post("/account/~delete/")
+        assert response.url == "/auth/login/"
+        assert User.objects.filter(username=login_user.username).count() == 0
