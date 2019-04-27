7c72da1a627e720e0d58f04e4df921ce05df22b7
danjac <danjac2018@gmail.com>
2019-04-27 12:48:40 +0300

    No need to save to db

--- communikit/content/tests/test_models.py
+++ communikit/content/tests/test_models.py
@@ -2,21 +2,20 @@ import pytest
 
 from django.utils.encoding import force_str
 
-from .factories import PostFactory
+# from .factories import PostFactory
+from communikit.content.models import Post
 
 pytestmark = pytest.mark.django_db
 
 
 class TestPostModel:
     def test_markdown(self):
-        post = PostFactory(description="*testing*")
+        post = Post(description="*testing*")
         assert force_str(post.markdown()) == "<p><em>testing</em></p>"
 
     def test_markdown_with_dangerous_tags(self):
-        post = PostFactory(
-            description="<script>alert('howdy');</script>"
-        )
+        post = Post(description="<script>alert('howdy');</script>")
         assert (
             force_str(post.markdown())
-            == "&lt;script&gt;alert('howdy');&lt;/script&gt;"  # noqa
+            == "&lt;script&gt;alert('howdy');&lt;/script&gt;"
         )
