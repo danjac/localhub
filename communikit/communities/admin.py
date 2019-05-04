9f6bc4cadeb6171061590bce78612e80d7f73d85
danjac <danjac2018@gmail.com>
2019-05-04 22:39:28 +0300

    Admin changes

--- communikit/communities/admin.py
+++ communikit/communities/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
 
-from communikit.communities.models import Community
+from communikit.communities.models import Community, Membership
 
 
+@admin.register(Community)
 class CommunityAdmin(admin.ModelAdmin):
     search_fields = ("domain", "name")
     list_display = ("domain", "name", "active")
 
 
-admin.site.register(Community, CommunityAdmin)
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ("community", "member", "role")
+    list_filter = ("active", "role")
+    search_fields = ("community__name", "member__username", "member__email")
+    raw_id_fields = ("member",)
+
+    def get_queryset(self, request: HttpRequest) -> HttpResponse:
+        return (
+            super().get_queryset(request).select_related("community", "member")
+        )
--- communikit/content/admin.py
+++ communikit/content/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
 
-# Register your models here.
+from markdownx.admin import MarkdownxModelAdmin
+
+from communikit.content.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(MarkdownxModelAdmin):
+    raw_id_fields = ("author",)
+    list_display = ("author", "community", "title", "created")
+    search_fields = (
+        "community__name",
+        "author__username",
+        "author__email",
+        "title",
+        "description",
+    )
+
+    def get_queryset(self, request: HttpRequest) -> HttpResponse:
+        return (
+            super().get_queryset(request).select_related("community", "author")
+        )
