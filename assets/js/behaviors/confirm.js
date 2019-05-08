21abdbf1cda8b14dc4013d01a4266121a248ca67
danjac <danjac2018@gmail.com>
2019-05-09 01:45:37 +0300

    Use single confirm dialog

--- assets/js/behaviors/confirm.js
+++ assets/js/behaviors/confirm.js
@@ -3,14 +3,26 @@ import onmount from 'onmount';
 onmount('[data-confirm-dialog]', function() {
   this.addEventListener('click', event => {
     const { target } = event;
-    const { confirmed, confirmDialog, confirmTrigger } = target.dataset;
+
+    const {
+      confirmed,
+      confirmTitle,
+      confirmContent,
+      confirmTrigger
+    } = target.dataset;
+
     if (confirmed) {
       delete target.dataset.confirmed;
       return true;
     }
     event.preventDefault();
-    const dialog = document.querySelector(confirmDialog);
-    dialog.classList.add('active');
+    const dialog = document.querySelector('#confirm-dialog');
+
+    dialog.querySelector('[data-confirm-title]').innerText = confirmTitle;
+    dialog.querySelector('[data-confirm-content]').innerText = confirmContent;
+
+    const handler = dialog.querySelector('[data-confirm-handler]');
+    handler.innerText = target.innerText;
 
     const confirmEvent = confirmTrigger
       ? new CustomEvent(confirmTrigger)
@@ -27,13 +39,13 @@ onmount('[data-confirm-dialog]', function() {
       }
     }
 
-    const handler = dialog.querySelector('[data-confirm-handler]');
-
     handler.addEventListener('click', onConfirm, { once: true });
 
     dialog.addEventListener('modal:close', () => {
       handler.removeEventListener('click', onConfirm, { once: true });
     });
+
+    dialog.classList.add('active');
 
     return false;
   });
