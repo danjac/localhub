56a68fe647151968424be1394ce7c2e3cbce1a5c
danjac <danjac2018@gmail.com>
2019-05-12 17:12:34 +0300

    Tidyup js

--- assets/js/behaviors/dropdown.js
+++
@@ -1,8 +0,0 @@
-import onmount from 'onmount';
-
-onmount('[data-dropdown-toggle]', function() {
-  this.addEventListener('click', event => {
-    event.preventDefault();
-    event.target.closest('.dropdown').classList.toggle('active');
-  });
-});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,14 @@
 /* global require */
 import axios from 'axios';
-// import onmount from 'onmount';
 import Turbolinks from 'turbolinks';
 
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
 
-// import './behaviors';
-
+// Axios setup
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/*
-axios.interceptors.response.use(data => {
-  onmount();
-  return data;
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  onmount();
-});
-
-document.addEventListener('turbolinks:load', () => {
-  onmount();
-});
-*/
 
 // Stimulus setup
 const application = Application.start();
