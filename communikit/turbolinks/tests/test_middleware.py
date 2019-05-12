4d344a7651a66639de801a8e200101dd319da8a1
danjac <danjac2018@gmail.com>
2019-05-12 20:40:49 +0300

    Middleware tests

--- communikit/turbolinks/tests/test_middleware.py
+++ communikit/turbolinks/tests/test_middleware.py
@@ -2,9 +2,19 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.test.client import RequestFactory
 
 from communikit.turbolinks.middleware import TurbolinksMiddleware
+from communikit.types import HttpRequestResponse
 
 
 class TestTurbolinksMiddleware:
+    def test_location_header(
+        self, req_factory: RequestFactory, get_response: HttpRequestResponse
+    ):
+        mw = TurbolinksMiddleware(get_response)
+        req = req_factory.get("/")
+        req.session = {"_turbolinks_redirect": "/"}
+        resp = mw(req)
+        assert resp["Turbolinks-Location"] == "/"
+
     def test_handle_redirect_if_turbolinks(self, req_factory: RequestFactory):
         def get_response(req: HttpRequest) -> HttpResponse:
             return HttpResponseRedirect("/")
@@ -17,3 +27,16 @@ class TestTurbolinksMiddleware:
         )
         resp = mw(req)
         assert resp["Content-Type"] == "text/javascript"
+
+    def test_handle_redirect_if_not_turbolinks(
+        self, req_factory: RequestFactory
+    ):
+        def get_response(req: HttpRequest) -> HttpResponse:
+            return HttpResponseRedirect("/")
+
+        mw = TurbolinksMiddleware(get_response)
+        req = req_factory.get("/")
+        req.session = {}
+        resp = mw(req)
+        assert resp["Location"] == "/"
+        assert req.session["_turbolinks_redirect"] == "/"
