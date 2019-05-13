858279afebb2ef01436f20e96c2657b8f7f4d769
danjac <danjac2018@gmail.com>
2019-05-14 00:25:25 +0300

    Form controller refactor

--- assets/js/controllers/form-controller.js
+++ assets/js/controllers/form-controller.js
@@ -5,19 +5,21 @@ import Turbolinks from 'turbolinks';
 export default class extends Controller {
   submit(event) {
     event.preventDefault();
+
     const method = this.element.getAttribute('method');
     const url = this.element.getAttribute('action');
-    const referrer = location.href;
+    const multipart =
+      this.element.getAttribute('enctype') === 'multipart/form-data';
 
-    const data = this.serialize();
+    const data = this.serialize(multipart);
 
-    this.element
-      .querySelectorAll('input,textarea')
-      .forEach(el => el.setAttribute('disabled', true));
+    this.formElements.forEach(el => el.setAttribute('disabled', true));
 
-    if (method === "GET") {
-      return Turbolinks.visit(`${url}?${data}`)
+    if (method === 'GET') {
+      return Turbolinks.visit(`${url}?${data}`);
     }
+
+    const referrer = location.href;
 
     axios({
       data,
@@ -27,12 +29,12 @@ export default class extends Controller {
       method,
       url
     }).then(response => {
-      this.element
-        .querySelectorAll('input,textarea')
-        .forEach(el => el.removeAttribute('disabled'));
+      this.formElements.forEach(el => el.removeAttribute('disabled'));
+
       const contentType = response.headers['content-type'];
-      // errors in form, re-render
+
       if (contentType.match(/html/)) {
+        // errors in form, re-render
         Turbolinks.controller.cache.put(
           referrer,
           Turbolinks.Snapshot.wrap(response.data)
@@ -43,57 +45,38 @@ export default class extends Controller {
         eval(response.data);
       }
     });
+    return false;
   }
 
-  // https://vanillajstoolkit.com/helpers/serialize/
-  // use FormData for multipart forms?
+  serialize(multipart) {
+    const data = multipart ? new FormData() : new URLSearchParams();
 
-  serialize() {
-    const serialized = [];
-
-    for (let i = 0; i < this.element.elements.length; i += 1) {
-      const field = this.element.elements[i];
-      if (!this.serializable(field)) {
-        continue;
-      }
-
-      if (field.type === 'select-multiple') {
-        for (let n = 0; n < field.options.length; n += 1) {
-          if (!field.options[n].selected) {
-            continue;
-          }
-          serialized.push(
-            encodeURIComponent(field.name) +
-              '=' +
-              encodeURIComponent(field.options[n].value)
-          );
+    this.formElements.forEach(field => {
+      if (field.name && !field.disabled) {
+        switch (field.type) {
+          case 'reset':
+          case 'button':
+          case 'submit':
+            break;
+          case 'file':
+            if (multipart) {
+              data.append(field.files[0]);
+            }
+            break;
+          case 'select-multiple':
+            field.options.filter(option => option.selected).forEach(option => {
+              data.append(field.name, option.value);
+            });
+            break;
+          default:
+            data.append(field.name, field.value);
         }
-      } else if (
-        (field.type !== 'checkbox' && field.type !== 'radio') ||
-        field.checked
-      ) {
-        // Convert field data to a query string
-        serialized.push(
-          encodeURIComponent(field.name) + '=' + encodeURIComponent(field.value)
-        );
       }
-    }
-
-    return serialized.join('&');
+    });
+    return data;
   }
 
-  serializable(field) {
-    // allow file type if multipart...
-    if (
-      !field.name ||
-      field.disabled ||
-      field.type === 'file' ||
-      field.type === 'reset' ||
-      field.type === 'submit' ||
-      field.type === 'button'
-    ) {
-      return false;
-    }
-    return true;
+  get formElements() {
+    return this.element.querySelectorAll('button,input,select,textarea');
   }
 }
