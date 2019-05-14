6ed7d432f57f0ca8553dfb4b53900bc101d72e6b
danjac <danjac2018@gmail.com>
2019-05-15 00:23:40 +0300

    Adding lat,lon fields

--- communikit/events/models.py
+++ communikit/events/models.py
@@ -7,7 +7,10 @@ class Event(Post):
 
     starts = models.DateTimeField()
     ends = models.DateTimeField(null=True, blank=True)
+
     location = models.TextField(blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.title or self.location
