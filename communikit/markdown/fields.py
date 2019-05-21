7c1b62d115d90062f171867315a2ef59ca2f045e
danjac <danjac2018@gmail.com>
2019-05-21 21:41:18 +0300

    More tidyup

--- communikit/markdown/fields.py
+++ communikit/markdown/fields.py
@@ -1,4 +1,7 @@
-from typing import Set, Optional
+# Copyright (c) 2019 by Dan Jacob
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from typing import Set, Optional, Type
 
 from django.db.models import Model, Field
 from django.utils.safestring import mark_safe
@@ -16,17 +19,21 @@ class MarkdownProxy(str):
         return extract_mentions(self)
 
 
-class MarkdownFieldDescriptor(object):
+class MarkdownFieldDescriptor:
     def __init__(self, field: Field):
         self.field = field
 
-    def __get__(self, instance=None, owner=None) -> Optional[MarkdownProxy]:
+    def __get__(
+        self,
+        instance: Optional[Model] = None,
+        owner: Optional[Type[Model]] = None,
+    ) -> Optional[MarkdownProxy]:
         value = instance.__dict__[self.field]
         if value is None:
             return value
         return MarkdownProxy(value)
 
-    def __set__(self, instance, value):
+    def __set__(self, instance: Model, value: Optional[str]):
         instance.__dict__[self.field] = value
 
 
