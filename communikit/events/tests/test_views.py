a7fdb5af1f4318be7bdce52ce23b156574e9a1b1
danjac <danjac2018@gmail.com>
2019-05-23 01:26:53 +0300

    Event tests

---
+++ communikit/events/tests/test_views.py
@@ -0,0 +1,127 @@
+import pytest
+
+from django.conf import settings
+from django.test.client import Client
+from django.urls import reverse
+
+from communikit.communities.models import Membership
+from communikit.events.models import Event
+from communikit.events.tests.factories import EventFactory
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def event_for_member(member: Membership) -> Event:
+    return EventFactory(owner=member.member, community=member.community)
+
+
+class TestEventCreateView:
+    def test_get(self, client: Client, member: Membership):
+        response = client.get(reverse("events:create"))
+        assert response.status_code == 200
+
+    def test_post(self, client: Client, member: Membership):
+        response = client.post(
+            reverse("events:create"),
+            {
+                "title": "test",
+                "description": "test",
+                "starts_0": "2019-2-2",
+                "starts_1": "10:00",
+                "ends_0": "2019-2-2",
+                "ends_1": "10:00",
+            },
+        )
+        assert response.url == reverse("events:list")
+        event = Event.objects.get()
+        assert event.owner == member.member
+        assert event.community == member.community
+
+
+class TestEventUpdateView:
+    def test_get(self, client: Client, event_for_member: Event):
+        response = client.get(
+            reverse("events:update", args=[event_for_member.id])
+        )
+        assert response.status_code == 200
+
+    def test_post(self, client: Client, event_for_member: Event):
+        response = client.post(
+            reverse("events:update", args=[event_for_member.id]),
+            {
+                "title": "UPDATED",
+                "description": event_for_member.description,
+                "starts_0": "2019-2-2",
+                "starts_1": "10:00",
+                "ends_0": "2019-2-2",
+                "ends_1": "10:00",
+            },
+        )
+        assert response.url == event_for_member.get_absolute_url()
+        event_for_member.refresh_from_db()
+        assert event_for_member.title == "UPDATED"
+
+
+class TestEventDeleteView:
+    def test_get(self, client: Client, event_for_member: Event):
+        # test confirmation page for non-JS clients
+        response = client.get(
+            reverse("events:delete", args=[event_for_member.id])
+        )
+        assert response.status_code == 200
+
+    def test_delete(self, client: Client, event_for_member: Event):
+        response = client.delete(
+            reverse("events:delete", args=[event_for_member.id])
+        )
+        assert response.url == reverse("events:list")
+        assert Event.objects.count() == 0
+
+
+class TestEventDetailView:
+    def test_get(self, client: Client, event: Event):
+        response = client.get(
+            reverse("events:detail", args=[event.id]),
+            HTTP_HOST=event.community.domain,
+        )
+        assert response.status_code == 200
+        assert "comment_form" not in response.context
+
+    def test_get_if_can_post_comment(
+        self,
+        client: Client,
+        event: Event,
+        login_user: settings.AUTH_USER_MODEL,
+    ):
+        Membership.objects.create(member=login_user, community=event.community)
+        response = client.get(
+            reverse("events:detail", args=[event.id]),
+            HTTP_HOST=event.community.domain,
+        )
+        assert response.status_code == 200
+        assert "comment_form" in response.context
+
+
+class TestEventLikeView:
+    def test_post(self, client: Client, member: Membership):
+        event = EventFactory(community=member.community)
+        response = client.post(
+            reverse("events:like", args=[event.id]),
+            HTTP_X_REQUESTED_WITH="XMLHttpRequest",
+        )
+        assert response.status_code == 204
+        like = event.like_set.get()
+        assert like.user == member.member
+
+
+class TestEventDislikeView:
+    def test_post(self, client: Client, member: Membership):
+        event = EventFactory(community=member.community)
+        event.like_set.create(user=member.member)
+        response = client.post(
+            reverse("events:dislike", args=[event.id]),
+            HTTP_X_REQUESTED_WITH="XMLHttpRequest",
+        )
+        assert response.status_code == 204
+        assert event.like_set.count() == 0
--- communikit/events/views.py
+++ communikit/events/views.py
@@ -1,4 +1,4 @@
-# # from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse_lazy
 
 from communikit.activities.views import (
     BaseActivityCreateView,
@@ -16,6 +16,7 @@ from communikit.events.models import Event
 class EventCreateView(BaseActivityCreateView):
     model = Event
     form_class = EventForm
+    success_url = reverse_lazy("events:list")
 
 
 event_create_view = EventCreateView.as_view()
@@ -45,6 +46,7 @@ event_update_view = EventUpdateView.as_view()
 
 class EventDeleteView(BaseActivityDeleteView):
     model = Event
+    success_url = reverse_lazy("events:list")
 
 
 event_delete_view = EventDeleteView.as_view()
