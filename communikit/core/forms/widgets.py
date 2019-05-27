3ce746f35303db4029e03e998db71bf522713f91
danjac <danjac2018@gmail.com>
2019-05-27 22:35:55 +0300

    Event form fixes

--- communikit/core/forms/widgets.py
+++ communikit/core/forms/widgets.py
@@ -1,12 +1,26 @@
 # Copyright (c) 2019 by Dan Jacob
 # SPDX-License-Identifier: AGPL-3.0-or-later
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class CalendarWidget(forms.SplitDateTimeWidget):
     template_name = "includes/forms/widgets/calendar.html"
+    input_type = "calendar"
 
     def __init__(self, *args, **kwargs):
         date_attrs = kwargs.pop("date_attrs", {})
-        date_attrs.update({"data-target": "calendar.dateInput"})
-        super().__init__(date_attrs=date_attrs, *args, **kwargs)
+        date_attrs.update(
+            {"data-target": "calendar.dateInput", "placeholder": _("Date")}
+        )
+        time_attrs = kwargs.pop("time_attrs", {})
+        time_attrs.update(
+            {"data-target": "calendar.timeInput", "placeholder": _("Time")}
+        )
+        super().__init__(
+            date_attrs=date_attrs,
+            time_attrs=time_attrs,
+            time_format="%H:%M",
+            *args,
+            **kwargs
+        )
