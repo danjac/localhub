9cd9becb4406d627c66416b8c8b1d3f961904eb9
danjac <danjac2018@gmail.com>
2019-06-06 15:31:04 +0300

    Testing pipeline

--- communikit/notifications/tests/test_views.py
+++ communikit/notifications/tests/test_views.py
@@ -1,10 +1,15 @@
+# Copyright (c) 2019 by Dan Jacob
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 import pytest
 
 from django.test.client import Client
 from django.urls import reverse
 
 from communikit.communities.models import Membership
+from communikit.events.tests.factories import EventFactory
 from communikit.notifications.models import Notification
+from communikit.photos.tests.factories import PhotoFactory
 from communikit.posts.tests.factories import PostFactory
 
 pytestmark = pytest.mark.django_db
@@ -20,8 +25,24 @@ class TestNotificationListView:
             community=post.community,
             verb="created",
         )
+        event = EventFactory(community=member.community)
+        Notification.objects.create(
+            content_object=event,
+            recipient=member.member,
+            actor=event.owner,
+            community=event.community,
+            verb="created",
+        )
+        photo = PhotoFactory(community=member.community)
+        Notification.objects.create(
+            content_object=event,
+            recipient=member.member,
+            actor=photo.owner,
+            community=photo.community,
+            verb="created",
+        )
         response = client.get(reverse("notifications:list"))
-        assert len(response.context["object_list"]) == 1
+        assert len(response.context["object_list"]) == 3
         assert response.status_code == 200
 
 
