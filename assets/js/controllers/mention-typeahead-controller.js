f115a68d03ea94c5a8a0fa6672942f78d1b2c2df
Dan Jacob <danjac2018@gmail.com>
2019-06-15 12:42:38 +0000

    Resolve "@mentions - ignore punctuation at end of mention"

--- assets/js/controllers/mention-typeahead-controller.js
+++ assets/js/controllers/mention-typeahead-controller.js
@@ -13,26 +13,42 @@ export default class extends Controller {
   }
 
   typeahead(event) {
-    // tbd: check for tab, if selection pick first result
     if (event.keyCode === TAB_KEY) {
-      this.handleSelection(this.selectorTarget.querySelector('li.menu-item a'));
-      return;
+      this.handleSelection(
+        this.selectorTarget.querySelector('[data-mention-typeahead-value]')
+      );
+      event.preventDefault();
+      return false;
     }
-    const { value } = this.inputTarget;
-    const index = value.lastIndexOf('@');
-    const text = value.slice(index + 1, value.length);
 
-    if (!text || text.indexOf(' ') > -1) {
+    const { value, selectionStart } = this.inputTarget;
+    const index = value.lastIndexOf('@', selectionStart) + 1;
+
+    if (index === 0) {
       this.closeSelector();
-      return;
+      return true;
     }
 
+    const text = value.slice(index, selectionStart);
+    if (!text || text.match(/[\s#<>!.?[\]|{}]+/)) {
+      this.closeSelector();
+      return true;
+    }
+
+    this.doSearch(text);
+    return false;
+  }
+
+  doSearch(text) {
     const searchUrl = this.data.get('search-url');
 
     axios.get(searchUrl, { params: { q: text } }).then(response => {
       if (response.data) {
         this.selectorTarget.innerHTML = response.data;
-        if (this.selectorTarget.querySelectorAll('li.menu-item').length) {
+        if (
+          this.selectorTarget.querySelectorAll('[data-mention-typeahead-value]')
+            .length
+        ) {
           this.openSelector();
         } else {
           this.closeSelector();
@@ -42,11 +58,26 @@ export default class extends Controller {
   }
 
   handleSelection(item) {
-    const value = item.getAttribute('data-mention-typeahead-value');
-    if (value) {
-      const index = this.inputTarget.value.lastIndexOf('@');
+    const mention = item && item.getAttribute('data-mention-typeahead-value');
+    if (mention) {
+      const { value, selectionStart } = this.inputTarget;
+      const index = value.lastIndexOf('@', selectionStart);
+      const tokens = value
+        .replace('\t', '')
+        .slice(index)
+        .split(/ /);
+      const [firstToken] = tokens;
+      const remainer = firstToken.match(/\n/)
+        ? firstToken.slice(firstToken.indexOf('\n'))
+        : '';
       this.inputTarget.value =
-        this.inputTarget.value.slice(0, index + 1) + value + ' ';
+        value.slice(0, index) +
+        '@' +
+        mention +
+        remainer +
+        ' ' +
+        tokens.slice(1).join(' ');
+      this.inputTarget.selectionEnd = index + mention.length + 1;
     }
     this.closeSelector();
   }
