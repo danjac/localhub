5d1f2823146f92c78aaf0e0a4756813c053f294f
danjac <danjac2018@gmail.com>
2019-07-05 15:17:30 +0300

    Handle embedded images

--- assets/js/controllers/oembed-controller.js
+++ assets/js/controllers/oembed-controller.js
@@ -5,10 +5,15 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
   connect() {
-    // if element contains any rich content (iframe, video, audio, img)
+    // if element contains any rich content (iframe, video, audio)
     // assign video-responsive class to the element.
-    if (this.element.querySelector('iframe, video, audio, img')) {
+    if (this.element.querySelector('iframe, video, audio')) {
       this.element.classList.add('video-responsive');
+    } else {
+      // if we just have embedded images, make these responsive.
+      this.element.querySelectorAll('img').forEach(el => {
+        el.classList.add('img-responsive');
+      });
     }
   }
 }
