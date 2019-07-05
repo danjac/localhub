d935352707ded8e6b8cf59b19ab6354abb271dae
danjac <danjac2018@gmail.com>
2019-07-06 01:06:52 +0300

    Added new fields

--- communikit/photos/models.py
+++ communikit/photos/models.py
@@ -5,8 +5,9 @@ from typing import Dict
 
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-from model_utils import FieldTracker
+from model_utils import Choices, FieldTracker
 
 from sorl.thumbnail import ImageField
 
@@ -19,18 +20,36 @@ from communikit.notifications.models import Notification
 
 class Photo(Activity):
 
+    CC_LICENSES = Choices(
+        ("by", _("Attribution")),
+        ("by-sa", _("Attribution ShareAlike")),
+        ("by-nd", _("Attribution NoDerivs")),
+        ("by-nc", _("Attribution NonCommercial")),
+        ("by-nc-sa", _("Attribution NonCommercial ShareAlike")),
+        ("by-nc-nd", _("Attribution NonCommercial NoDerivs")),
+    )
+
     title = models.CharField(max_length=300)
     image = ImageField(upload_to="photos")
 
-    description_tracker = FieldTracker(["description"])
+    artist = models.CharField(max_length=100, blank=True)
+    original_url = models.URLField(null=True, blank=True)
+    cc_license = models.CharField(
+        max_length=10, choices=CC_LICENSES, null=True, blank=True
+    )
 
     comments = GenericRelation(Comment, related_query_name="photo")
     flags = GenericRelation(Flag, related_query_name="photo")
     likes = GenericRelation(Like, related_query_name="photo")
     notifications = GenericRelation(Notification, related_query_name="photo")
 
+    description_tracker = FieldTracker(["description"])
+
     def __str__(self) -> str:
         return self.title
 
     def search_index_components(self) -> Dict[str, str]:
         return {"A": self.title, "B": self.description}
+
+    def has_attribution(self) -> bool:
+        return any((self.artist, self.original_url, self.cc_license))
