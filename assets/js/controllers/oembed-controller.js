1395860a2f02d4f5618602508105e72417a8ea6e
danjac <danjac2018@gmail.com>
2019-07-05 15:50:09 +0300

    Ignore script tags

--- assets/js/controllers/oembed-controller.js
+++ assets/js/controllers/oembed-controller.js
@@ -7,7 +7,11 @@ export default class extends Controller {
   connect() {
     // if element contains any rich content (iframe, video, audio)
     // assign video-responsive class to the element.
-    if (this.element.querySelector('iframe, video, audio')) {
+    // ignore if there's a <script> tag as we don't want to break loading
+    if (
+      this.element.querySelector('iframe, video, audio') &&
+      !this.element.querySelector('script')
+    ) {
       this.element.classList.add('video-responsive');
     } else {
       // if we just have embedded images, make these responsive.
