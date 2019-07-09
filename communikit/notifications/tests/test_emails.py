84a530b539d918943aae46ae584198b5454bcdef
danjac <danjac2018@gmail.com>
2019-07-09 22:12:12 +0300

    Fix for notification emails

---
+++ communikit/notifications/tests/test_emails.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2019 by Dan Jacob
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import pytest
+
+from django.core import mail
+
+from communikit.comments.models import Comment
+from communikit.events.models import Event
+from communikit.notifications.emails import send_notification_email
+from communikit.notifications.models import Notification
+from communikit.photos.models import Photo
+from communikit.posts.models import Post
+from communikit.users.tests.factories import UserFactory
+
+pytestmark = pytest.mark.django_db
+
+
+class TestSendNotificationEmail:
+    def test_send_emails(
+        self, comment: Comment, event: Event, post: Post, photo: Photo
+    ):
+        recipient = UserFactory()
+
+        for item, template_name in [
+            (comment, "comments/emails/notification.txt"),
+            (event, "events/emails/notification.txt"),
+            (post, "posts/emails/notification.txt"),
+            (photo, "photos/emails/notification.txt"),
+        ]:
+            notification = Notification.objects.create(
+                recipient=recipient,
+                verb="created",
+                actor=item.owner,
+                community=item.community,
+                content_object=item,
+            )
+            send_notification_email(
+                notification, "test", item.get_permalink(), template_name
+            )
+
+        assert len(mail.outbox) == 4
