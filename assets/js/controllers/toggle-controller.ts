e67718677a6f37ea26dd4ff7d29b60246b9ef028
danjac <danjac2018@gmail.com>
2019-07-18 12:31:53 +0300

    Toggle controller

--- assets/js/controllers/toggle-controller.ts
+++ assets/js/controllers/toggle-controller.ts
@@ -1,13 +1,19 @@
 // Copyright (c) 2019 by Dan Jacob
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+/* global NodeListOf */
+
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
   static targets = ['togglable'];
 
-  toggle(event) {
+  togglableTargets: NodeListOf<HTMLElement>;
+
+  toggle(event: Event) {
     event.preventDefault();
-    this.togglableTargets.forEach(el => el.classList.toggle('d-hide'));
+    this.togglableTargets.forEach((el: HTMLElement) =>
+      el.classList.toggle('d-hide')
+    );
   }
 }
