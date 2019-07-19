69947d30792734f159a243c771d4dea9b7f0d4eb
danjac <danjac2018@gmail.com>
2019-07-19 14:43:26 +0300

    Adding dependencies

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -6,12 +6,11 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: Home
     },
     {
       path: '/about',
@@ -19,7 +18,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
 });
