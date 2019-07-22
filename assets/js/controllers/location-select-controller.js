cacdbce3f0a86460c76f356de5f2b37fd1553470
danjac <danjac2018@gmail.com>
2019-07-22 13:31:02 +0300

    Use correct action syntax

--- assets/js/controllers/location-select-controller.js
+++ assets/js/controllers/location-select-controller.js
@@ -2,12 +2,13 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { Controller } from 'stimulus';
+import Turbolinks from 'turbolinks';
 
 export default class extends Controller {
   select() {
     const { value } = this.element;
     if (value) {
-      window.location.href = value;
+      Turbolinks.visit(value);
       this.element.disabled = true;
     }
   }
