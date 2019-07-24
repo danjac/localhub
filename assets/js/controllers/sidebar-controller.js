6b39094068cd220b15a2d7dcbff163bfb65ce11f
danjac <danjac2018@gmail.com>
2019-07-24 16:53:03 +0300

    Increase sidebar hide width

--- assets/js/controllers/sidebar-controller.js
+++ assets/js/controllers/sidebar-controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
 
   toggle(event) {
     event.preventDefault();
-    this.mainTarget.classList.toggle('hide-md');
-    this.navTarget.classList.toggle('hide-md');
+    this.mainTarget.classList.toggle('hide-lg');
+    this.navTarget.classList.toggle('hide-lg');
   }
 }
