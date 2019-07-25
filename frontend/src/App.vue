913ccc43f08f1c096672fc0a130f80ef92ac0c4a
danjac <danjac2018@gmail.com>
2019-07-25 17:01:56 +0300

    Copy of home page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,17 +3,53 @@
     <header class="navbar bg-dark px-2 py-1 my-2">
       <section class="navbar-section">
         <a class="navbar-brand mr-2 text-light" href="/">
-          Localhub
+          LocalHub
         </a>
       </section>
-      <section class="navbar-section">
-        <router-link class="btn btn-link text-light" to="/">Home</router-link>
-        <router-link class="btn btn-link text-light" to="/about"
-          >About</router-link
+
+      <section class="navbar-section hide-sm">
+        <form
+          method="GET"
+          action="/search/"
+          data-controller="form"
+          data-action="form#submit"
         >
+          <div class="input-group input-inline">
+            <input
+              class="form-input"
+              name="q"
+              type="search"
+              placeholder="Search"
+            />
+            <button class="btn bg-gray text-dark input-group-btn">
+              Search
+            </button>
+          </div>
+        </form>
+      </section>
+
+      <section class="navbar-section">
+        <a class="btn btn-link text-light" href="/people/danjac/">
+          <figure class="avatar avatar-sm">
+            <img
+              src="https://communikit.s3.amazonaws.com/media/cache/1c/cd/1ccd95dd491da83dd8424896db2a85ea.jpg"
+              alt="Dan Jacob"
+              width="100"
+              height="100"
+            />
+          </figure>
+
+          Dan Jacob
+        </a>
+        <a class="btn btn-link text-light" href="/account/logout/">Logout</a>
       </section>
     </header>
-
     <router-view />
   </div>
 </template>
+
+<style lang="scss">
+.conversation-input {
+  height: 2rem;
+}
+</style>
