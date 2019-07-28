f5fc7eaf21254b18963bbca5d0084bb253d9ba3b
danjac <danjac2018@gmail.com>
2019-07-29 00:58:50 +0300

    URL invite fix

--- localhub/invites/urls.py
+++ localhub/invites/urls.py
@@ -14,7 +14,7 @@ app_name = "invites"
 urlpatterns = [
     path("", view=invite_list_view, name="list"),
     path("~create/", view=invite_create_view, name="create"),
-    path("~<pk>/~resend/", view=invite_resend_view, name="resend"),
-    path("~<pk>/~delete/", view=invite_delete_view, name="delete"),
-    path("~<pk>/~accept/", view=invite_accept_view, name="accept"),
+    path("<pk>/~resend/", view=invite_resend_view, name="resend"),
+    path("<pk>/~delete/", view=invite_delete_view, name="delete"),
+    path("<pk>/~accept/", view=invite_accept_view, name="accept"),
 ]
