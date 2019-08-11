54e3c9936cac6c7d7749619f15443e981521e12b
danjac <danjac2018@gmail.com>
2019-08-11 12:21:30 +0300

    Membership delete email copy changes

--- localhub/communities/emails.py
+++ localhub/communities/emails.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.translation import gettext as _
+from django.utils.translation import override
 
 from localhub.communities.models import Community
 
@@ -13,13 +14,17 @@ from localhub.communities.models import Community
 def send_membership_deleted_email(
     member: settings.AUTH_USER_MODEL, community: Community
 ):
-    context = {"member": member, "community": community}
-    send_mail(
-        _("Your membership of community %s has been deleted") % community,
-        render_to_string("communities/emails/membership_deleted.txt", context),
-        community.resolve_email("support"),
-        [member.email],
-        html_message=render_to_string(
-            "communities/emails/membership_deleted.html", context
-        ),
-    )
+    with override(member.language):
+        context = {"member": member, "community": community}
+        send_mail(
+            _("Your membership of community %(community)s has been terminated")
+            % {"community": community.name},
+            render_to_string(
+                "communities/emails/membership_deleted.txt", context
+            ),
+            community.resolve_email("support"),
+            [member.email],
+            html_message=render_to_string(
+                "communities/emails/membership_deleted.html", context
+            ),
+        )
