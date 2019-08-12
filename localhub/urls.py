b1c088021384dea849f471fb995e8c7c143ed641
danjac <danjac2018@gmail.com>
2019-08-12 18:09:11 +0300

    change network url

--- localhub/urls.py
+++ localhub/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("posts/", include("localhub.posts.urls")),
     path("people/", include("localhub.users.urls")),
     path("community/", include("localhub.communities.urls")),
-    path("directory/", community_list_view, name="community_list"),
+    path("network/", community_list_view, name="community_list"),
     path("account/~update", user_update_view, name="user_update"),
     path("account/~delete", user_delete_view, name="user_delete"),
     path(
