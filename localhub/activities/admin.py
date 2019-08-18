96d4f1f354d8459aafb674d11629f750424ca509
danjac <danjac2018@gmail.com>
2019-08-18 10:29:22 +0300

    Improved admin stuff

--- localhub/activities/admin.py
+++ localhub/activities/admin.py
@@ -1,12 +1,33 @@
 # Copyright (c) 2019 by Dan Jacob
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+from typing import Tuple
+
+from django.db.models import QuerySet
+from django.http import HttpRequest
+
 from simple_history.admin import SimpleHistoryAdmin
 
 from localhub.core.markdown.admin import MarkdownFieldMixin
 
 
 class ActivityAdmin(MarkdownFieldMixin, SimpleHistoryAdmin):
-    raw_id_fields = ("owner", "editor")
-    list_display = ("__str__", "owner", "community", "created")
+
+    autocomplete_fields = ("owner", "editor", "community")
+    date_hierarchy = "created"
+    list_display = ("__str__", "owner", "community", "created", "is_reshare")
+    list_filter = ("community",)
+    raw_id_fields = ("parent",)
     ordering = ("-created",)
+    search_fields = ("search_document", "owner__username")
+
+    def get_search_results(
+        self, request: HttpRequest, queryset: QuerySet, search_term: str
+    ) -> Tuple[QuerySet, bool]:
+        if search_term:
+            return (
+                queryset.search(search_term)
+                | queryset.filter(owner__username__icontains=search_term),
+                True,
+            )
+        return super().get_search_results(request, queryset, search_term)
