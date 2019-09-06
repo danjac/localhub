ee223c30356d69790c911842f01afa830b3948b9
danjac <danjac2018@gmail.com>
2019-09-06 14:02:06 +0300

    Refactor join requests

--- localhub/join_requests/admin.py
+++ localhub/join_requests/admin.py
@@ -9,5 +9,5 @@ from localhub.join_requests.models import JoinRequest
 @admin.register(JoinRequest)
 class JoinRequestAdmin(admin.ModelAdmin):
     raw_id_fields = ("sender",)
-    list_display = ("email", "sender", "community", "status")
+    list_display = ("sender", "community", "status")
     ordering = ("-sent",)
