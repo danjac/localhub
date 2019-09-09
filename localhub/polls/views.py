edbf198fb67ecc9f4f40093913f0885e8c917189
danjac <danjac2018@gmail.com>
2019-09-09 21:42:54 +0300

    Refactor poll views

--- localhub/polls/views.py
+++ localhub/polls/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms import inlineformset_factory
 from django.shortcuts import redirect
+from django.utils.functional import cached_property
 from django.utils.translation import gettext as _
 
 
@@ -39,23 +40,18 @@ class PollQuerySetMixin:
 class PollCreateView(ActivityCreateView):
     model = Poll
 
-    def get(self, request):
-        self.answers_formset = AnswersFormSet()
-        return super().get(request)
-
-    def form_invalid(self, form, answers_formset=None):
-        self.answers_formset = answers_formset or AnswersFormSet(
-            self.request.POST
-        )
-        return super().form_invalid(form)
+    @cached_property
+    def answers_formset(self):
+        if self.request.method == "POST":
+            return AnswersFormSet(self.request.POST)
+        return AnswersFormSet()
 
     def form_valid(self, form):
-        answers_formset = AnswersFormSet(self.request.POST)
-        if not answers_formset.is_valid():
-            return self.form_invalid(form, answers_formset)
+        if not self.answers_formset.is_valid():
+            return self.form_invalid(form)
         response = super().form_valid(form)
-        answers_formset.instance = self.object
-        answers_formset.save()
+        self.answers_formset.instance = self.object
+        self.answers_formset.save()
         return response
 
     def get_context_data(self, **kwargs):
@@ -65,27 +61,18 @@ class PollCreateView(ActivityCreateView):
 
 
 class PollUpdateView(ActivityUpdateView):
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        self.answers_formset = AnswersFormSet(instance=self.object)
-        form = self.get_form(instance=self.object)
-        context = self.get_context_data(form=form)
-        return self.render_to_response(context)
 
-    def form_invalid(self, form, answers_formset=None):
-        self.answers_formset = answers_formset or AnswersFormSet(
-            self.request.POST, instance=self.object
-        )
-        return super().form_invalid(form)
+    @cached_property
+    def answers_formset(self):
+        if self.request.method == "POST":
+            return AnswersFormSet(self.request.POST, instance=self.object)
+        return AnswersFormSet(instance=self.object)
 
     def form_valid(self, form):
-        answers_formset = AnswersFormSet(
-            self.request.POST, instance=self.object
-        )
-        if not answers_formset.is_valid():
-            return self.form_invalid(form, answers_formset)
+        if not self.answers_formset.is_valid():
+            return self.form_invalid(form)
         response = super().form_valid(form)
-        answers_formset.save()
+        self.answers_formset.save()
         return response
 
     def get_context_data(self, **kwargs):
