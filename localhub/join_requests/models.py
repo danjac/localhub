861ae28cfdc467e473abea59717c908e8cf563f6
danjac <danjac2018@gmail.com>
2019-11-10 18:51:30 +0200

    Remove unused import

--- localhub/join_requests/models.py
+++ localhub/join_requests/models.py
@@ -4,7 +4,6 @@
 
 from django.db import models
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 from model_utils import Choices
