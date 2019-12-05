907cbc3ce6eb37d307431345812b34aee765c2f5
danjac <danjac2018@gmail.com>
2019-12-05 13:58:16 +0200

    Remove drafts from user activities view

--- localhub/users/views.py
+++ localhub/users/views.py
@@ -236,6 +236,7 @@ class UserStreamView(SingleUserMixin, BaseStreamView):
             super()
             .filter_queryset(queryset)
             .without_blocked_tags(self.request.user)
+            .published()
             .filter(owner=self.user_obj)
         )
 
