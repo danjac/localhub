9ab0a08d2a3c21128dd3da6b688a5f2969f32b85
danjac <danjac2018@gmail.com>
2019-12-15 14:42:25 +0200

    Sort out search params

--- localhub/views/search.py
+++ localhub/views/search.py
@@ -10,3 +10,23 @@ class SearchMixin:
     @cached_property
     def search_query(self):
         return self.request.GET.get(self.search_query_parameter, "")
+
+    def get_context_data(self, **kwargs):
+        """
+        Add query string wihout search query parameter
+        """
+        data = super().get_context_data(**kwargs)
+        non_search_params = self.request.GET.copy()
+        for param in ("page", self.search_query_parameter):
+            if param in non_search_params:
+                del non_search_params[param]
+        non_search_path = self.request.path
+        if non_search_params:
+            non_search_path += "?" + non_search_params.urlencode()
+        data.update(
+            {
+                "non_search_params": non_search_params,
+                "non_search_path": non_search_path,
+            }
+        )
+        return data
