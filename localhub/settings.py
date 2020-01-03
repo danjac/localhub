890719d5ae2921a33ecc79f612f3b6c76554ebab
danjac <danjac2018@gmail.com>
2020-01-03 12:02:51 +0200

    Updated security settings

--- localhub/settings.py
+++ localhub/settings.py
@@ -318,10 +318,14 @@ class Production(Base):
 
     # Security settings
     SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
-    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_SECONDS = 86400  # 1 day
     SECURE_SSL_REDIRECT = True
+
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
 
     DEFAULT_FILE_STORAGE = "localhub.storages.MediaStorage"
     STATICFILES_STORAGE = "localhub.storages.StaticStorage"
