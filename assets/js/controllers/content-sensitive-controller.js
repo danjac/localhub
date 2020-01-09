29e002c8f71d86373a09d67b2ec774d754387ce9
danjac <danjac2018@gmail.com>
2020-01-09 20:20:51 +0200

    Handle sensitive content

--- assets/js/controllers/content-sensitive-controller.js
+++ assets/js/controllers/content-sensitive-controller.js
@@ -1,13 +1,20 @@
 // Copyright (c) 2019 by Dan Jacob
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { Controller } from 'stimulus';
+import {
+  Controller
+} from 'stimulus';
 
 export default class extends Controller {
   static targets = ['togglable', 'warning'];
 
   toggle(event) {
     event.preventDefault();
-    this.togglableTargets.forEach(el => el.classList.toggle('d-hide'));
+    this.togglableTargets.forEach(el => {
+      el.classList.toggle("d-hide");
+      for (const child of el.getElementsByClassName("collapsed")) {
+        child.classList.toggle("collapsed");
+      }
+    });
   }
 }
