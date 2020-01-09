4b3d55420929303c35b07db7269a90548dc64b39
danjac <danjac2018@gmail.com>
2020-01-09 15:46:00 +0200

    Collapsable descriptions

---
+++ assets/js/controllers/collapsable-controller.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2019 by Dan Jacob
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import {
+  Controller
+} from 'stimulus';
+
+export default class extends Controller {
+  static targets = ['container', 'toggle'];
+
+  connect() {
+    if (this.containerTarget.offsetHeight < this.containerTarget.scrollHeight) {
+      this.toggleTarget.classList.toggle('d-hide');
+    }
+  }
+
+  toggle(event) {
+    event.preventDefault();
+    this.containerTarget.classList.toggle('collapsed');
+    this.toggleTarget.classList.toggle('d-hide');
+  }
+}
