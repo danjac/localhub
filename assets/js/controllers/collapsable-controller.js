122dccbc4e670d4235bcd83f2b8f0fc43b9299f1
danjac <danjac2018@gmail.com>
2020-01-09 17:25:26 +0200

    Add debug info

--- assets/js/controllers/collapsable-controller.js
+++ assets/js/controllers/collapsable-controller.js
@@ -11,6 +11,8 @@ export default class extends Controller {
   connect() {
     // reload container from DOM to get "true" heights
     const container = document.getElementById(this.containerTarget.id);
+    console.log('container', container.offsetHeight, container.scrollHeight);
+
     if (container && container.offsetHeight < container.scrollHeight) {
       this.toggleTarget.classList.remove('d-hide');
     }
