b5be7f106a612f4b52e8ecb45e5139d964544e70
danjac <danjac2018@gmail.com>
2020-01-16 13:50:24 +0200

    Revert "Typo fix"
    This reverts commit d6a7e5fc41d6c63f17a812e51437809674b9f638.

--- assets/js/controllers/collapsable-controller.js
+++ assets/js/controllers/collapsable-controller.js
@@ -30,7 +30,7 @@ export default class extends Controller {
     if (this.isCollapsableHeight) {
       this.makeCollapsable();
     } else {
-      this.removeCollapsable();
+      this.removeCollapable();
     }
   }
 
