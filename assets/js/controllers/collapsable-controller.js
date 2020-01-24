9e0c6016b7ef093b395a26c6a6e8f820b1b99895
danjac <danjac2018@gmail.com>
2020-01-24 20:47:46 +0200

    Tidy up

--- assets/js/controllers/collapsable-controller.js
+++ assets/js/controllers/collapsable-controller.js
@@ -10,59 +10,28 @@ const MAX_HEIGHT = 500;
 export default class extends Controller {
   static targets = ['container', 'toggle'];
 
-  /*
-  initialize() {
-    document.addEventListener(
-      'turbolinks:render', () => this.checkContainerHeights()
-    )
-  }
-  */
-
   connect() {
-    this.checkContainerHeights();
-  }
-
-  toggle(event) {
-    event.preventDefault();
-    this.removeCollapsable();
-  }
-
-  checkContainerHeight() {
-    // show "show more" button if container higher than max height
-    console.log('clientHeight', this.containerTarget.clientHeight);
-    console.log('offsetHeight', this.containerTarget.offsetHeight);
-    console.log('scrollHeight', this.containerTarget.scrollHeight);
-    if (this.isCollapsableHeight) {
-      this.makeCollapsable();
-    } else {
-      // this.removeCollapsable();
-    }
-  }
-
-  checkContainerHeights() {
-    this.checkContainerHeight();
+    this.makeCollapsable();
     // ensure we check heights of all images and other dynamic elements
     // and handle when these are individually loaded
     for (const tag of ["img", "iframe"]) {
       for (const el of this.containerTarget.getElementsByTagName(tag)) {
-        el.onload = () => this.checkContainerHeight();
+        el.onload = () => this.makeCollapsable();
       }
     }
-
   }
 
-  makeCollapsable() {
-    this.containerTarget.classList.add('collapsable');
-    this.toggleTargets.forEach(el => el.classList.remove('d-hide'));
-  }
-
-  removeCollapsable() {
+  toggle(event) {
+    event.preventDefault();
     this.containerTarget.classList.remove('collapsable');
     this.toggleTargets.forEach(el => el.classList.add('d-hide'));
   }
 
-  get isCollapsableHeight() {
-    return this.containerTarget.clientHeight > MAX_HEIGHT
-    // return this.containerTarget.scrollHeight > MAX_HEIGHT
+  makeCollapsable() {
+    // show "show more" button if container higher than max height
+    if (this.containerTarget.clientHeight > MAX_HEIGHT) {
+      this.containerTarget.classList.add('collapsable');
+      this.toggleTargets.forEach(el => el.classList.remove('d-hide'));
+    }
   }
 }
