ac9374a26400b67839a7992db392dfe46a379a2f
danjac <danjac2018@gmail.com>
2020-02-04 09:02:09 +0200

    Handle multiple badge counters

--- assets/js/controllers/badge-controller.js
+++ assets/js/controllers/badge-controller.js
@@ -7,21 +7,19 @@ import {
 
 export default class extends Controller {
   decrement() {
-    const id = this.data.get('id');
-    if (!id) {
-      return;
-    }
-
-    const el = document.getElementById(id);
-    const value = el && el.dataset.badge;
-    if (value) {
-      const newValue = parseInt(value, 10) - 1;
-      if (newValue) {
-        el.dataset.badge = newValue;
-      } else {
-        delete el.dataset.badge;
-        el.classList.remove('badge');
+    const counters = (this.data.get('counters') || '').split(',');
+    counters.forEach(counter => {
+      const el = document.getElementById(counter.trim());
+      const value = el && el.dataset.badge;
+      if (value) {
+        const newValue = parseInt(value, 10) - 1;
+        if (newValue) {
+          el.dataset.badge = newValue;
+        } else {
+          delete el.dataset.badge;
+          el.classList.remove('badge');
+        }
       }
-    }
+    })
   }
 }
