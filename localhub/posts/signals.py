0006a7a9ca107b6070a9a745612fe482b94943c3
danjac <danjac2018@gmail.com>
2020-02-09 18:41:13 +0200

    Move opengraph update to post_save signal

--- localhub/posts/signals.py
+++ localhub/posts/signals.py
@@ -1,15 +1,15 @@
 # Copyright (c) 2019 by Dan Jacob
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Post
 
 
 @receiver(
-    pre_save, sender=Post, dispatch_uid="posts.fetch_post_opengraph_data_from_url"
+    post_save, sender=Post, dispatch_uid="posts.fetch_post_opengraph_data_from_url"
 )
-def fetch_post_opengraph_data_from_url(instance, **kwargs):
-    if instance._state.adding or instance.url_tracker.changed():
-        instance.fetch_opengraph_data_from_url(commit=False)
+def fetch_post_opengraph_data_from_url(instance, created=False, **kwargs):
+    if created or instance.url_tracker.changed():
+        instance.fetch_opengraph_data_from_url()
