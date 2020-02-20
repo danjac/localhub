fc9bac2371b31746388e0c736d90bb841b0d7a99
danjac <danjac2018@gmail.com>
2020-02-21 00:28:57 +0200

    Simplify cookie notices

--- assets/js/controllers/cookie-notice-controller.js
+++ assets/js/controllers/cookie-notice-controller.js
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import Cookies from 'js-cookie';
-import { Controller } from 'stimulus';
+import {
+  Controller
+} from 'stimulus';
 
 export default class extends Controller {
   connect() {
@@ -13,7 +15,7 @@ export default class extends Controller {
 
   accept() {
     Cookies.set(this.data.get('name'), true, {
-      domain: this.data.get('domain'),
+      domain: window.location.hostname,
       expires: 360
     });
     this.element.remove();
