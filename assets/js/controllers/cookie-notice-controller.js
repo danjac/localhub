62e27655441d6bd4a46a2afdab491dc4941bade9
Dan Jacob <danjac2018@gmail.com>
2020-02-29 16:52:48 +0200

    Use user name in membership info

--- assets/js/controllers/cookie-notice-controller.js
+++ assets/js/controllers/cookie-notice-controller.js
@@ -2,9 +2,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import Cookies from 'js-cookie';
-import {
-  Controller
-} from 'stimulus';
+import { Controller } from 'stimulus';
 
 export default class extends Controller {
   connect() {
