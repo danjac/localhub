d4fd6f6e3b0efbcd65b0e43e9a6790bcc82b35fa
Dan Jacob <danjac2018@gmail.com>
2020-03-01 00:24:12 +0200

    Tidying up search links

--- assets/js/controllers/active-link-controller.js
+++ assets/js/controllers/active-link-controller.js
@@ -1,20 +1,24 @@
 // Copyright (c) 2019 by Dan Jacob
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { Controller } from 'stimulus';
+import {
+  Controller
+} from 'stimulus';
 
 export default class extends Controller {
   static targets = ['match'];
 
   connect() {
-    const { pathname } = window.location;
-    // if element is a link i.e. <a> then just use that href
+    const {
+      pathname,
+      search
+    } = window.location;
     const href =
-      this.element.getAttribute('href') ||
-      this.matchTarget.getAttribute('href');
-    const matches = this.data.has('exact')
-      ? pathname === href
-      : pathname.startsWith(href.split(/[?#]/)[0]);
+      (this.element.getAttribute('href') ||
+        this.matchTarget.getAttribute('href')).split(/[?#]/)[0];
+    const matches = this.data.has('exact') ?
+      pathname === href :
+      pathname.startsWith(href);
     if (matches) {
       this.element.classList.add(this.data.get('active-class') || 'active');
     }
