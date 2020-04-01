382182ad4d7c0c7290c17a75be7a295f59fbc21d
Dan Jacob <danjac2018@gmail.com>
2020-04-01 14:57:27 +0300

    Space things out a bit

--- assets/js/controllers/oembed-controller.js
+++ assets/js/controllers/oembed-controller.js
@@ -10,7 +10,9 @@ export default class extends Controller {
   Renders oembed element depending on source e.g. an image, video, audio,
   iframe etc. Ensures the correct CSS class is applied. This is handled
   on page load once the actual element is rendered so we know how to
-  apply the correct class.
+  apply the correct class. If no such element is embedded then the whole
+  element is removed.
+
   */
   connect() {
     if (
@@ -20,9 +22,14 @@ export default class extends Controller {
       this.element.classList.add('video-responsive');
     } else {
       // if we just have embedded images, make these responsive.
-      this.element.querySelectorAll('img').forEach(el => {
-        el.classList.add('img-responsive');
-      });
+      const images = this.element.querySelectorAll('img');
+      if (images.length > 0) {
+        images.forEach(el => {
+          el.classList.add('img-responsive');
+        });
+      } else {
+        this.element.remove();
+      }
     }
   }
 }
