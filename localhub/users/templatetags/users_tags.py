aced2270f71f2bc1e33094e2bb12c392b7102d90
Dan Jacob <danjac2018@gmail.com>
2020-04-06 13:14:37 +0300

    More sophisticated tag

--- localhub/users/templatetags/users_tags.py
+++ localhub/users/templatetags/users_tags.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from django import template
+from django.template.base import token_kwargs
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -24,7 +26,70 @@ def dismissable_notice(user, notice, text, css_class=None):
     return {
         "user": user,
         "notice": notice,
-        "text": text,
+        "dismissable_content": text,
         "css_class": css_class,
         "show_notice": show_notice,
     }
+
+
+@register.tag
+def dismissable(parser, token):
+    """
+    Renders a notice dismissable by the user.
+
+    Example:
+
+    {% dismissable user "private-stash" "toast-primary" %}
+    my notice content goes here...
+    {% enddismissable %}
+    """
+    bits = token.split_contents()[1:]
+
+    try:
+        user, notice = bits[:2]
+    except ValueError:
+        raise template.TemplateSyntaxError("user and notice must be provided")
+
+    extra_context = token_kwargs(bits[2:], parser)
+
+    nodelist = parser.parse(("enddismissable",))
+    parser.delete_first_token()
+
+    return DismissableNode(user, notice, nodelist, extra_context=extra_context)
+
+
+class DismissableNode(template.Node):
+    template = "users/includes/dismissable_notice.html"
+
+    def __init__(self, user, notice, nodelist, extra_context=None):
+
+        self.user = template.Variable(user)
+        self.notice = template.Variable(notice)
+        self.extra_context = extra_context or {}
+        self.nodelist = nodelist
+
+    def render(self, context):
+
+        user = self.user.resolve(context)
+        notice = self.notice.resolve(context)
+
+        show_notice = user.is_anonymous or notice not in user.dismissed_notices
+
+        if show_notice:
+            context.update({"user": user, "notice": notice})
+            context.push(
+                {
+                    key: value.resolve(context)
+                    for key, value in self.extra_context.items()
+                }
+            )
+            dismissable_content = mark_safe(self.nodelist.render(context))
+            context.update(
+                {
+                    "dismissable_content": dismissable_content,
+                    "show_notice": True,  # TBD: remove
+                }
+            )
+            return template.loader.render_to_string(self.template, context.flatten())
+
+        return ""
--- localhub/users/tests/test_templatetags.py
+++ localhub/users/tests/test_templatetags.py
@@ -4,9 +4,10 @@
 import pytest
 
 from django.contrib.auth.models import AnonymousUser
+from django.template import engines
 
 from ..factories import UserFactory
-from ..templatetags.users_tags import avatar, dismissable_notice
+from ..templatetags.users_tags import avatar
 
 pytestmark = pytest.mark.django_db
 
@@ -23,16 +24,26 @@ class TestAvatar:
         assert context["initials"] == "T"
 
 
-class TestDismissableNotice:
+class TestDismissable:
+    template = """
+    {% load users_tags %}
+    {% dismissable user "private-stash" "toast-primary" %}
+    This is your Private Stash
+    {% enddismissable %}
+    """
+
+    def render_template(self, context):
+        return engines["django"].from_string(self.template).render(context)
+
     def test_if_anonymous(self):
-        context = dismissable_notice(AnonymousUser(), "private-stash", "text")
-        assert context["show_notice"]
+        content = self.render_template({"user": AnonymousUser()})
+        assert "This is your Private Stash" in content
 
     def test_if_not_dismissed(self, user):
-        context = dismissable_notice(user, "private-stash", "text")
-        assert context["show_notice"]
+        content = self.render_template({"user": user})
+        assert "This is your Private Stash" in content
 
     def test_if__dismissed(self, user):
         user.dismissed_notices.append("private-stash")
-        context = dismissable_notice(user, "private-stash", "text")
-        assert not context["show_notice"]
+        content = self.render_template({"user": user})
+        assert "This is your Private Stash" not in content
