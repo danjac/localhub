815a65d3431e2a900199dfb108a3f34337c57511
Dan Jacob <danjac2018@gmail.com>
2020-04-07 14:46:07 +0300

    More controller tweaks

--- assets/js/controllers/toggle-controller.js
+++ assets/js/controllers/toggle-controller.js
@@ -5,15 +5,13 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
   /*
-  Toggles one or more elements using d-hide class.
+  Toggles one or more elements using d-hide class or another class.
 
   actions:
-    toggle: toggles d-hide on all togglable targets. The trigger element
-      should include a data-toggle-target attribute which specifies the
-      element(s) to be toggled.
+    toggle: toggles d-hide on all togglable targets.
 
   targets:
-    togglable: an element to be toggled.
+    togglable: elements to be toggled.
   */
   static targets = ['togglable'];
 
