9fd6eef081161757bd1815d1ddeb1c2920edace0
Dan Jacob <danjac2018@gmail.com>
2020-04-08 16:45:05 +0300

    Code tidyup

--- localhub/polls/migrations/0010_auto_20200408_1326.py
+++ localhub/polls/migrations/0010_auto_20200408_1326.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polls', '0009_auto_20200407_2251'),
+        ("polls", "0009_auto_20200407_2251"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='historicalpoll',
-            name='allow_voting',
+            model_name="historicalpoll",
+            name="allow_voting",
             field=models.BooleanField(default=True),
         ),
         migrations.AddField(
-            model_name='poll',
-            name='allow_voting',
+            model_name="poll",
+            name="allow_voting",
             field=models.BooleanField(default=True),
         ),
     ]
