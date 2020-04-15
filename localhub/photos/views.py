74d561d47a3121874f7b5274430f9eb795e6f90c
Dan Jacob <danjac2018@gmail.com>
2020-04-15 13:25:12 +0300

    Optimize gallery queries

--- localhub/photos/views.py
+++ localhub/photos/views.py
@@ -14,7 +14,11 @@ class PhotoGalleryView(ActivityListView):
     paginate_by = settings.LOCALHUB_LONG_PAGE_SIZE
 
     def get_queryset(self):
-        return super().get_queryset().filter(parent__isnull=True)
+        return (
+            Photo.objects.for_community(self.request.community)
+            .published_or_owner(self.request.user)
+            .exclude_blocked(self.request.user)
+        )
 
 
 photo_gallery_view = PhotoGalleryView.as_view()
