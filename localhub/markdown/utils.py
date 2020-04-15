2c91358811c6918a52ac76e7a8a223ab6947c65e
Dan Jacob <danjac2018@gmail.com>
2020-04-16 01:14:55 +0300

    Add more TLDs to linkifier

--- localhub/markdown/utils.py
+++ localhub/markdown/utils.py
@@ -9,7 +9,7 @@ from functools import partial
 
 import bleach
 from bleach import Cleaner  # type: ignore
-from bleach.linkifier import LinkifyFilter
+from bleach.linkifier import LinkifyFilter, TLDS, build_url_re
 from markdownx.utils import markdownify as default_markdownify
 
 from localhub.hashtags.utils import linkify_hashtags
@@ -40,6 +40,22 @@ ALLOWED_TAGS = bleach.ALLOWED_TAGS + [
 ALLOWED_ATTRIBUTES = bleach.ALLOWED_ATTRIBUTES.copy()
 ALLOWED_ATTRIBUTES.update({"img": ["alt", "src"], "a": ["rel", "target", "href"]})
 
+ADDITIONAL_TLDS = """app audio auto bar bargains best bid bike blog bot
+                     car center cern cheap click cloud codes coffee college
+                     coop dance data date dating directory download eco
+                     education email events exchange faith farm game gay gift
+                     global google guru help hiphop hosting house icu info ink
+                     international jobs land lgbt life link lol love map market
+                     meet menu mobi movie museum music name ngo onl ooo organic
+                     party photo photos pics pizzas porn post pro property pub
+                     review rocks sale science sex sexy shop social solar store
+                     stream sucks support tattoo technology tel today top trade
+                     travel ventures video voting webcam wedding wiki win wtf
+                     xxx xyz
+                  """.split()
+
+URL_RE = build_url_re(TLDS + ADDITIONAL_TLDS)
+
 
 def set_link_target(attrs, new=False):
     # ignore any internal links
@@ -66,5 +82,5 @@ def markdownify(content):
 cleaner = Cleaner(
     tags=ALLOWED_TAGS,
     attributes=ALLOWED_ATTRIBUTES,
-    filters=[partial(LinkifyFilter, callbacks=[set_link_target])],
+    filters=[partial(LinkifyFilter, callbacks=[set_link_target], url_re=URL_RE)],
 )
