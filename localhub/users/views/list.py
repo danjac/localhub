ad51043ddee95c98748092afc80c9410f153d10a
Dan Jacob <danjac2018@gmail.com>
2020-04-16 21:47:50 +0300

    Tidy up user list views

--- localhub/users/views/list.py
+++ localhub/users/views/list.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from django.conf import settings
-from django.db.models import BooleanField, Q, Value
+from django.db.models import Q
 from vanilla import ListView
 
 from localhub.views import SearchMixin
@@ -13,73 +13,33 @@ from .mixins import UserQuerySetMixin
 class BaseUserListView(UserQuerySetMixin, ListView):
     paginate_by = settings.LOCALHUB_LONG_PAGE_SIZE
 
+    exclude_blocked = True
+
+    include_unread_messages = True
+    include_membership_details = True
+
     def get_queryset(self):
-        return (
+        qs = (
             super()
             .get_queryset()
             .with_joined(self.request.community)
-            .with_role(self.request.community)
-            .order_by("name", "username")
             .exclude(blocked=self.request.user)
+            .order_by("name", "username")
         )
+        if self.include_membership_details:
+            qs = qs.with_role(self.request.community).with_is_following(
+                self.request.user
+            )
+        if self.exclude_blocked:
+            qs = qs.exclude(blockers=self.request.user)
+        if self.include_unread_messages:
+            qs = qs.with_num_unread_messages(self.request.user, self.request.community)
+        return qs
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         data["blocked_users"] = self.request.user.get_blocked_users()
         return data
-
-
-class FollowingUserListView(BaseUserListView):
-    template_name = "users/following_user_list.html"
-
-    def get_queryset(self):
-        return (
-            self.request.user.following.annotate(
-                is_following=Value(True, output_field=BooleanField())
-            )
-            .for_community(self.request.community)
-            .with_role(self.request.community)
-            .with_num_unread_messages(self.request.user, self.request.community)
-            .order_by("name", "username")
-        )
-
-
-following_user_list_view = FollowingUserListView.as_view()
-
-
-class FollowerUserListView(BaseUserListView):
-    template_name = "users/follower_user_list.html"
-
-    def get_queryset(self):
-        return (
-            super()
-            .get_queryset()
-            .filter(following=self.request.user)
-            .for_community(self.request.community)
-            .with_role(self.request.community)
-            .with_is_following(self.request.user)
-            .with_num_unread_messages(self.request.user, self.request.community)
-        )
-
-
-follower_user_list_view = FollowerUserListView.as_view()
-
-
-class BlockedUserListView(BaseUserListView):
-    template_name = "users/blocked_user_list.html"
-
-    def get_queryset(self):
-        return (
-            super()
-            .get_queryset()
-            .filter(blockers=self.request.user)
-            .for_community(self.request.community)
-            .with_role(self.request.community)
-            .with_is_following(self.request.user)
-        )
-
-
-blocked_user_list_view = BlockedUserListView.as_view()
 
 
 class MemberListView(SearchMixin, BaseUserListView):
@@ -90,15 +50,7 @@ class MemberListView(SearchMixin, BaseUserListView):
     template_name = "users/member_list.html"
 
     def get_queryset(self):
-        qs = (
-            super()
-            .get_queryset()
-            .exclude(blocked=self.request.user)
-            .for_community(self.request.community)
-            .with_role(self.request.community)
-            .with_is_following(self.request.user)
-            .with_num_unread_messages(self.request.user, self.request.community)
-        )
+        qs = super().get_queryset()
         if self.search_query:
             qs = qs.search(self.search_query)
         return qs
@@ -107,11 +59,45 @@ class MemberListView(SearchMixin, BaseUserListView):
 member_list_view = MemberListView.as_view()
 
 
+class FollowingUserListView(BaseUserListView):
+    template_name = "users/following_user_list.html"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(followers=self.request.user)
+
+
+following_user_list_view = FollowingUserListView.as_view()
+
+
+class FollowerUserListView(BaseUserListView):
+    template_name = "users/follower_user_list.html"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(following=self.request.user)
+
+
+follower_user_list_view = FollowerUserListView.as_view()
+
+
+class BlockedUserListView(BaseUserListView):
+    template_name = "users/blocked_user_list.html"
+    include_unread_messages = False
+    exclude_blocked = False
+
+    def get_queryset(self):
+        return super().get_queryset().filter(blockers=self.request.user)
+
+
+blocked_user_list_view = BlockedUserListView.as_view()
+
+
 class UserAutocompleteListView(BaseUserListView):
+    include_unread_messages = False
+    include_membership_details = False
     template_name = "users/user_autocomplete_list.html"
 
     def get_queryset(self):
-        qs = super().get_queryset().exclude(blocked=self.request.user)
+        qs = super().get_queryset()
         search_term = self.request.GET.get("q", "").strip()
         if search_term:
             return qs.filter(
