e947a61e8ddf4d05de95fbbadaaa3eb6e8486750
Dan Jacob <danjac2018@gmail.com>
2020-04-22 15:54:03 +0300

    Comments update

--- localhub/template/defaultfilters.py
+++ localhub/template/defaultfilters.py
@@ -34,12 +34,12 @@ def resolve_url(model, view_name):
     Examples:
 
         {% resolve_url Post 'list' %} -> /posts/
+        {% resolve_url post 'list' %} -> /posts/
         {% resolve_url post 'update' %} -> /posts/1234/~update/
 
     Args:
         model (Model class or instance)
         view_name (str): name of the view
-        **kwargs: URL kwargs
 
     Returns:
         str: url
