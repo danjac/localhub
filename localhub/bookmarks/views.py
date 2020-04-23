4a7540068cd922aa6f3dc0739f45baf682af7685
Dan Jacob <danjac2018@gmail.com>
2020-04-23 10:07:32 +0300

    Add search and pagination to bookmarks

--- localhub/bookmarks/views.py
+++ localhub/bookmarks/views.py
@@ -1,42 +1,62 @@
 # Copyright (c) 2020 by Dan Jacob
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+from django.conf import settings
 from django.views.generic import ListView
 
 from localhub.activities.views.streams import BaseActivityStreamView
 from localhub.comments.views.list import BaseCommentListView
 from localhub.private_messages.views.mixins import SenderOrRecipientQuerySetMixin
+from localhub.views import SearchMixin
 
 
-class BookmarksStreamView(BaseActivityStreamView):
+class BookmarksStreamView(SearchMixin, BaseActivityStreamView):
     template_name = "bookmarks/activities.html"
     ordering = ("-bookmarked", "-created")
-
-    def get_count_queryset_for_model(self, model):
-        return self.filter_queryset(
-            model.objects.published_or_owner(self.request.user).bookmarked(
-                self.request.user
-            )
-        )
+    paginate_by = settings.LOCALHUB_DEFAULT_PAGE_SIZE
 
     def filter_queryset(self, queryset):
-        return (
+        qs = (
             super()
             .filter_queryset(queryset)
             .published_or_owner(self.request.user)
             .bookmarked(self.request.user)
             .with_bookmarked_timestamp(self.request.user)
         )
+        if self.search_query:
+            qs = qs.search(self.search_query)
+        return qs
 
 
 bookmarks_stream_view = BookmarksStreamView.as_view()
 
 
-class BookmarksCommentListView(BaseCommentListView):
-    template_name = "bookmarks/comments.html"
+class BookmarksMessageListView(SenderOrRecipientQuerySetMixin, SearchMixin, ListView):
+    template_name = "bookmarks/messages.html"
+    paginate_by = settings.LOCALHUB_DEFAULT_PAGE_SIZE
 
     def get_queryset(self):
-        return (
+        qs = (
+            super()
+            .get_queryset()
+            .bookmarked(self.request.user)
+            .with_bookmarked_timestamp(self.request.user)
+            .order_by("-bookmarked", "-created")
+        )
+        if self.search_query:
+            qs = qs.search(self.search_query)
+        return qs
+
+
+bookmarks_message_list_view = BookmarksMessageListView.as_view()
+
+
+class BookmarksCommentListView(SearchMixin, BaseCommentListView):
+    template_name = "bookmarks/comments.html"
+    paginate_by = settings.LOCALHUB_DEFAULT_PAGE_SIZE
+
+    def get_queryset(self):
+        qs = (
             super()
             .get_queryset()
             .bookmarked(self.request.user)
@@ -44,23 +64,9 @@ class BookmarksCommentListView(BaseCommentListView):
             .with_bookmarked_timestamp(self.request.user)
             .order_by("-bookmarked", "-created")
         )
+        if self.search_query:
+            qs = qs.search(self.search_query)
+        return qs
 
 
 bookmarks_comment_list_view = BookmarksCommentListView.as_view()
-
-
-class BookmarksMessageListView(SenderOrRecipientQuerySetMixin, ListView):
-
-    template_name = "bookmarks/messages.html"
-
-    def get_queryset(self):
-        return (
-            super()
-            .get_queryset()
-            .bookmarked(self.request.user)
-            .with_bookmarked_timestamp(self.request.user)
-            .order_by("-bookmarked", "-created")
-        )
-
-
-bookmarks_message_list_view = BookmarksMessageListView.as_view()
