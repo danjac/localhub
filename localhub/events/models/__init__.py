18e14aca2a143e7d32eb74020d593496ba52b9f5
Dan Jacob <danjac2018@gmail.com>
2020-04-23 18:00:31 +0300

    Break up events models

--- localhub/events/models/__init__.py
+++ localhub/events/models/__init__.py
@@ -14,87 +14,12 @@ from icalendar import Calendar
 from icalendar import Event as CalendarEvent
 from timezone_field import TimeZoneField
 
-from localhub.activities.models import Activity, ActivityQuerySet
+from localhub.activities.models import Activity
 from localhub.db.search import SearchIndexer
-from localhub.db.utils import boolean_value
 from localhub.notifications.decorators import dispatch
 from localhub.utils.http import get_domain
 
-
-class EventQuerySet(ActivityQuerySet):
-    def is_attending(self, user):
-        """Annotates "is_attending" if user is attending the event.
-
-        Args:
-            user (User)
-
-        Returns:
-            QuerySet
-        """
-        return self.annotate(
-            is_attending=boolean_value(False)
-            if user.is_anonymous
-            else models.Exists(
-                self.model.objects.filter(attendees=user, pk=models.OuterRef("pk"))
-            )
-        )
-
-    def with_num_attendees(self):
-        """Annotates "num_attendees" to each event.
-
-        Returns:
-            QuerySet
-        """
-        return self.annotate(num_attendees=models.Count("attendees"))
-
-    def with_relevance(self):
-        """
-        Annotates "relevance" value based on start date and/or status.
-
-        Relevance:
-        1) if private/canceled: -1
-        2) if event coming up: +1
-        3) if event passed: 0
-
-        Returns:
-            QuerySet
-        """
-
-        return self.annotate(
-            relevance=models.Case(
-                models.When(
-                    models.Q(published__isnull=True) | models.Q(canceled__isnull=False),
-                    then=models.Value(-1),
-                ),
-                models.When(starts__gte=timezone.now(), then=models.Value(1)),
-                default=0,
-                output_field=models.IntegerField(),
-            )
-        )
-
-    def with_timedelta(self):
-        """
-        Adds a DurationField with distance between now and the start time (future or past).
-
-        Returns:
-            QuerySet
-        """
-        now = timezone.now()
-        return self.annotate(
-            timedelta=models.Case(
-                models.When(starts__gte=now, then=models.F("starts") - now),
-                models.When(starts__lt=now, then=now - models.F("starts")),
-                output_field=models.DurationField(),
-            )
-        )
-
-    def with_common_annotations(self, user, community):
-        return (
-            super()
-            .with_common_annotations(user, community)
-            .with_num_attendees()
-            .is_attending(user)
-        )
+from .querysets import EventQuerySet
 
 
 class Event(Activity):
---
+++ localhub/events/models/querysets.py
@@ -0,0 +1,84 @@
+# Copyright (c) 2020 by Dan Jacob
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from django.db import models
+from django.utils import timezone
+
+from localhub.activities.models.querysets import ActivityQuerySet
+from localhub.db.utils import boolean_value
+
+
+class EventQuerySet(ActivityQuerySet):
+    def is_attending(self, user):
+        """Annotates "is_attending" if user is attending the event.
+
+        Args:
+            user (User)
+
+        Returns:
+            QuerySet
+        """
+        return self.annotate(
+            is_attending=boolean_value(False)
+            if user.is_anonymous
+            else models.Exists(
+                self.model.objects.filter(attendees=user, pk=models.OuterRef("pk"))
+            )
+        )
+
+    def with_num_attendees(self):
+        """Annotates "num_attendees" to each event.
+
+        Returns:
+            QuerySet
+        """
+        return self.annotate(num_attendees=models.Count("attendees"))
+
+    def with_relevance(self):
+        """
+        Annotates "relevance" value based on start date and/or status.
+
+        Relevance:
+        1) if private/canceled: -1
+        2) if event coming up: +1
+        3) if event passed: 0
+
+        Returns:
+            QuerySet
+        """
+
+        return self.annotate(
+            relevance=models.Case(
+                models.When(
+                    models.Q(published__isnull=True) | models.Q(canceled__isnull=False),
+                    then=models.Value(-1),
+                ),
+                models.When(starts__gte=timezone.now(), then=models.Value(1)),
+                default=0,
+                output_field=models.IntegerField(),
+            )
+        )
+
+    def with_timedelta(self):
+        """
+        Adds a DurationField with distance between now and the start time (future or past).
+
+        Returns:
+            QuerySet
+        """
+        now = timezone.now()
+        return self.annotate(
+            timedelta=models.Case(
+                models.When(starts__gte=now, then=models.F("starts") - now),
+                models.When(starts__lt=now, then=now - models.F("starts")),
+                output_field=models.DurationField(),
+            )
+        )
+
+    def with_common_annotations(self, user, community):
+        return (
+            super()
+            .with_common_annotations(user, community)
+            .with_num_attendees()
+            .is_attending(user)
+        )
