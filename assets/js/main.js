7c328674482518e441453a7d742c815b6e5b3f57
Dan Jacob <danjac2018@gmail.com>
2020-04-28 02:21:39 +0300

    Use data-turblinks false for ajax actions

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@
 /* global require */
 import axios from 'axios';
 import Turbolinks from 'turbolinks';
+import hoverintent from 'hoverintent';
 
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
@@ -17,6 +18,44 @@ axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 const application = Application.start();
 const context = require.context('./controllers', true, /\.js$/);
 application.load(definitionsFromContext(context));
+
+// instant click setup
+
+document.addEventListener('turbolinks:load', () => {
+  document.querySelectorAll('a').forEach((el) => {
+    // TBD: all those AJAX <a> elements should be data-turbolinks="false"
+    if (el.dataset.turbolinks === 'false' || el.dataset.controller === 'ajax') {
+      return;
+    }
+
+    let prefetcher;
+
+    hoverintent(
+      el,
+      () => {
+        const href = el.getAttribute('href');
+        if (!href.match(/^\//)) {
+          return;
+        }
+        if (prefetcher) {
+          if (prefetcher.getAttribute('href') !== href) {
+            prefetcher.setAttribute('href', href);
+          }
+        } else {
+          const link = document.createElement('link');
+          link.setAttribute('rel', 'prefetch');
+          link.setAttribute('href', href);
+          prefetcher = document.body.appendChild(link);
+        }
+      },
+      () => {},
+      {
+        options: 50,
+        sensitivity: 5,
+      }
+    );
+  });
+});
 
 // Turbolinks setup
 Turbolinks.start();
