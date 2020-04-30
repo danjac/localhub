43da2e6ef1eb5c3fa0c90c63f4ecf63255c22984
Dan Jacob <danjac2018@gmail.com>
2020-04-30 17:43:40 +0300

    URL updates

--- localhub/apps/join_requests/urls.py
+++ localhub/apps/join_requests/urls.py
@@ -3,20 +3,16 @@
 
 from django.urls import path
 
-from .views.actions import join_request_accept_view, join_request_reject_view
-from .views.create import join_request_create_view
-from .views.delete import join_request_delete_view
-from .views.detail import join_request_detail_view
-from .views.list import join_request_list_view, sent_join_request_list_view
+from . import views
 
 app_name = "join_requests"
 
 urlpatterns = [
-    path("", view=join_request_list_view, name="list"),
-    path("sent/", view=sent_join_request_list_view, name="sent_list"),
-    path("~create/", view=join_request_create_view, name="create"),
-    path("<int:pk>/", view=join_request_detail_view, name="detail"),
-    path("<int:pk>/~accept/", view=join_request_accept_view, name="accept"),
-    path("<int:pk>/~reject/", view=join_request_reject_view, name="reject"),
-    path("<int:pk>/~delete/", view=join_request_delete_view, name="delete"),
+    path("", views.join_request_list_view, name="list"),
+    path("sent/", views.sent_join_request_list_view, name="sent_list"),
+    path("~create/", views.join_request_create_view, name="create"),
+    path("<int:pk>/", views.join_request_detail_view, name="detail"),
+    path("<int:pk>/~accept/", views.join_request_accept_view, name="accept"),
+    path("<int:pk>/~reject/", views.join_request_reject_view, name="reject"),
+    path("<int:pk>/~delete/", views.join_request_delete_view, name="delete"),
 ]
