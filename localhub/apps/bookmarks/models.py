804e5ddc658eea18ae1548b792f4cdcca50bf8d6
Dan Jacob <danjac2018@gmail.com>
2020-04-30 16:16:24 +0300

    Merging bookmarks models

--- localhub/apps/bookmarks/models.py
+++ localhub/apps/bookmarks/models.py
@@ -1,15 +1,19 @@
 # Copyright (c) 2020 by Dan Jacob
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from model_utils.models import TimeStampedModel
+
+from localhub.apps.communities.models import Community
 from localhub.common.db.generic import (
     get_generic_related_exists,
     get_generic_related_value_subquery,
 )
 from localhub.common.db.utils import boolean_value
-
-from . import Bookmark
 
 
 class BookmarkAnnotationsQuerySetMixin:
@@ -86,3 +90,36 @@ class BookmarkAnnotationsQuerySetMixin:
                 )
             }
         )
+
+
+class BookmarkQuerySet(models.QuerySet):
+    def for_models(self, *models):
+        """
+        Returns instances of a Bookmark for a given set of models.
+        """
+        return self.filter(
+            content_type__in=ContentType.objects.get_for_models(*models).values()
+        )
+
+
+class Bookmark(TimeStampedModel):
+
+    community = models.ForeignKey(Community, related_name="+", on_delete=models.CASCADE)
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="+", on_delete=models.CASCADE
+    )
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    objects = BookmarkQuerySet.as_manager()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "content_type", "object_id"], name="unique_bookmark",
+            )
+        ]
+        indexes = [models.Index(fields=["content_type", "object_id"])]
