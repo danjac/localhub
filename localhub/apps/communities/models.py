67123b74ce700b91ab65fbbd5cdb1097eac26132
Dan Jacob <danjac2018@gmail.com>
2020-04-30 16:33:40 +0300

    Merge models

--- localhub/apps/communities/models.py
+++ localhub/apps/communities/models.py
@@ -1,11 +1,15 @@
 # Copyright (c) 2020 by Dan Jacob
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+from dataclasses import dataclass
 from urllib.parse import urljoin
 
+from django.apps import apps
 from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.validators import RegexValidator, URLValidator
 from django.db import models
+from django.http import HttpRequest
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -13,13 +17,110 @@ from model_utils.models import TimeStampedModel
 from sorl.thumbnail import ImageField
 
 from localhub.apps.hashtags.utils import extract_hashtags
+from localhub.common.db.search.mixins import SearchQuerySetMixin
 from localhub.common.markdown.fields import MarkdownField
-
-from .managers import CommunityManager, MembershipManager
 
 DOMAIN_VALIDATOR = RegexValidator(
     regex=URLValidator.host_re, message=_("This is not a valid domain")
 )
+
+
+class MembershipQuerySet(SearchQuerySetMixin, models.QuerySet):
+    search_document_field = "member__search_document"
+
+
+class MembershipManager(models.Manager.from_queryset(MembershipQuerySet)):
+    ...
+
+
+class CommunityQuerySet(models.QuerySet):
+    def with_num_members(self):
+        return self.annotate(num_members=models.Count("membership"))
+
+    def with_is_member(self, user):
+        if user.is_authenticated:
+            return self.annotate(
+                is_member=models.Exists(
+                    self.model.objects.filter(
+                        membership__member=user,
+                        membership__active=True,
+                        membership__community=models.OuterRef("pk"),
+                    )
+                ),
+                member_role=models.Subquery(
+                    apps.get_model("communities.Membership")
+                    .objects.filter(
+                        member=user, active=True, community=models.OuterRef("pk"),
+                    )
+                    .values("role")[:1],
+                ),
+            )
+        return self.annotate(
+            is_member=models.Value(False, output_field=models.BooleanField()),
+            member_role=models.Value(None, output_field=models.CharField()),
+        )
+
+    def accessible(self, user):
+        """
+        Returns all communities either listed publicly or where user is a member
+
+        Args:
+            user (User)
+
+        Returns:
+            QuerySet
+        """
+        return (
+            self.with_is_member(user)
+            .filter(models.Q(models.Q(is_member=True) | models.Q(public=True)))
+            .distinct()
+        )
+
+
+class CommunityManager(models.Manager.from_queryset(CommunityQuerySet)):
+    def get_current(self, request):
+        """
+        Returns current community matching request domain if active.
+        """
+        try:
+            return self.get(active=True, domain__iexact=request.get_host())
+        except self.model.DoesNotExist:
+            site = get_current_site(request)
+            return RequestCommunity(request, site.name, site.domain)
+
+
+@dataclass
+class RequestCommunity:
+    """
+    This works in a similar way to Django auth AnonymousUser, if
+    no community present. It provides ducktyping so we don't have to check
+    for None everywhere. Wraps HttpRequest/Site.
+    """
+
+    request: HttpRequest
+
+    name: str
+    domain: str
+
+    id = None
+    pk = None
+
+    active: bool = False
+
+    def get_absolute_url(self):
+        return self.request.full_path
+
+    def user_has_role(self, user, *roles):
+        return False
+
+    def is_member(self, user):
+        return False
+
+    def is_moderator(self, user):
+        return False
+
+    def is_admin(self, user):
+        return False
 
 
 class Community(TimeStampedModel):
