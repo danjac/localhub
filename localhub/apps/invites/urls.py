362b70e6c2ab2e1ebb3d8d430bb0c211a516bec0
Dan Jacob <danjac2018@gmail.com>
2020-04-30 17:16:06 +0300

    URL updates

--- localhub/apps/invites/urls.py
+++ localhub/apps/invites/urls.py
@@ -3,22 +3,18 @@
 
 from django.urls import path
 
-from .views.actions import invite_accept_view, invite_reject_view, invite_resend_view
-from .views.create import invite_create_view
-from .views.delete import invite_delete_view
-from .views.detail import invite_detail_view
-from .views.list import invite_list_view, received_invite_list_view
+from . import views
 
 app_name = "invites"
 
 
 urlpatterns = [
-    path("", view=invite_list_view, name="list"),
-    path("received/", view=received_invite_list_view, name="received_list"),
-    path("~create/", view=invite_create_view, name="create"),
-    path("<pk>/~resend/", view=invite_resend_view, name="resend"),
-    path("<pk>/~delete/", view=invite_delete_view, name="delete"),
-    path("<pk>/~accept/", view=invite_accept_view, name="accept"),
-    path("<pk>/~reject/", view=invite_reject_view, name="reject"),
-    path("<pk>/", view=invite_detail_view, name="detail"),
+    path("", views.invite_list_view, name="list"),
+    path("received/", views.received_invite_list_view, name="received_list"),
+    path("~create/", views.invite_create_view, name="create"),
+    path("<pk>/~resend/", views.invite_resend_view, name="resend"),
+    path("<pk>/~delete/", views.invite_delete_view, name="delete"),
+    path("<pk>/~accept/", views.invite_accept_view, name="accept"),
+    path("<pk>/~reject/", views.invite_reject_view, name="reject"),
+    path("<pk>/", views.invite_detail_view, name="detail"),
 ]
