1a567701112b6982efcbee48cc2263a86e263afe
Dan Jacob <danjac2018@gmail.com>
2020-04-30 16:45:12 +0300

    Merge models

--- localhub/apps/flags/models.py
+++ localhub/apps/flags/models.py
@@ -12,6 +12,33 @@ from model_utils.models import TimeStampedModel
 from localhub.apps.communities.models import Community
 from localhub.apps.notifications.decorators import dispatch
 from localhub.apps.notifications.models import Notification
+from localhub.common.db.generic import get_generic_related_exists
+
+
+class FlagAnnotationsQuerySetMixin:
+    """
+    Adds annotation methods to related model query set.
+    """
+
+    def with_is_flagged(self, annotated_name="is_flagged"):
+        """
+        Adds True if the object has been flagged by a user.
+        """
+        return self.annotate(
+            **{annotated_name: get_generic_related_exists(self.model, Flag)}
+        )
+
+    def with_has_flagged(self, user, annotated_name="has_flagged"):
+        """
+        Adds True if the user in question has flagged the object.
+        """
+        return self.annotate(
+            **{
+                annotated_name: get_generic_related_exists(
+                    self.model, Flag.objects.filter(user=user)
+                )
+            }
+        )
 
 
 class Flag(TimeStampedModel):
