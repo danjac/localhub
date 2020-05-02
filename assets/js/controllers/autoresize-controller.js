2afd60d36898f0c073e662da949ac0c992023f62
Dan Jacob <danjac2018@gmail.com>
2020-05-03 02:22:50 +0300

    Add autoresizer

---
+++ assets/js/controllers/autoresize-controller.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 by Dan Jacob
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import ApplicationController from './application-controller';
+
+export default class extends ApplicationController {
+  connect() {
+    this.setToScrollHeight();
+    this.element.style.overflowY = 'hidden';
+  }
+
+  // data-action="autoresize->keyup#resize"
+  resize() {
+    this.element.style.height = 'auto';
+    this.setToScrollHeight();
+  }
+
+  setToScrollHeight() {
+    this.element.style.height = this.element.scrollHeight + 'px';
+  }
+}
