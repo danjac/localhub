dea54206958c48c99b746cf0fd472076e220c2f9
Dan Jacob <danjac2018@gmail.com>
2020-05-26 00:37:45 +0300

    vue setup

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -20,14 +20,6 @@
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
           target="_blank"
           rel="noopener"
--- frontend/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  outputDir: 'dist',
-  assetsDir: 'static',
-  indexPath: 'frontend.html',
-};
