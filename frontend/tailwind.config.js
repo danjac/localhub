e92be2bfa64d6b308206b296376ef2eac11ea9a7
Dan Jacob <danjac2018@gmail.com>
2020-05-26 00:49:31 +0300

    Tailwind integration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 by Dan Jacob
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/* eslint-disable */
+module.exports = {
+  purge: {
+    content: ['./src/**/*.vue'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
