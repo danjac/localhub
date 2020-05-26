4c3d4cb2aaad64e5b039501f3d3f4c530bf6b7c1
Dan Jacob <danjac2018@gmail.com>
2020-05-26 23:20:28 +0300

    Loading bar

---
+++ frontend/src/components/LoadingBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="shadow w-full bg-gray-100">
+    <div
+      class="bg-blue-900 text-xs leading-none py-2 my:auto text-center text-white"
+      :style="{ width: `${loadingState}%` }"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingBar',
+  data() {
+    return {
+      loadingState: 0,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.loadingState += 5;
+      if (this.loadingState > 100) {
+        this.loadingState = 0;
+      }
+    }, 50);
+  },
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App!!!" />
+    <LoadingBar v-if="loading" />
+    {{ pageObj }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
+import LoadingBar from '@/components/LoadingBar.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    LoadingBar,
+  },
+  data() {
+    return {
+      loading: false,
+      error: false,
+      loadingState: 0,
+      pageObj: {
+        results: [],
+        next: null,
+        prev: null,
+      },
+    };
+  },
+  async created() {
+    this.loading = true;
+    this.error = false;
+    try {
+      const response = await axios.get('/api/streams/default/');
+      this.pageObj = response.data;
+    } catch (e) {
+      this.error = true;
+    } finally {
+      this.loading = false;
+    }
   },
 };
 </script>
