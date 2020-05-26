c192deb05757d1ea57d56d872b5b0ebc79e2fe5a
Dan Jacob <danjac2018@gmail.com>
2020-05-26 17:39:21 +0300

    Login page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,37 @@
+<template> </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      invalidLogin: false,
+    };
+  },
+  methods: {
+    async authenticate() {
+      this.invalidLogin = false;
+      const data = new URLSearchParams();
+      data.append("login", this.username);
+      data.append("password", this.password);
+      try {
+        const response = await axios({
+          data,
+          url: "/api/auth/login/",
+          method: "POST",
+        });
+        if (response.status === 200) {
+          window.location.href = "/";
+        } else if (response.status === 400) {
+          this.invalidLogin = true;
+        }
+      } catch (e) {
+        this.invalidLogin = true;
+      }
+    },
+  },
+};
+</script>
