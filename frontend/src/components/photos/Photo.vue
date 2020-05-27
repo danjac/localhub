9faff75f889e9cb0ca026441c5b70b8fd77ef543
Dan Jacob <danjac2018@gmail.com>
2020-05-27 15:49:36 +0300

    Photo component

--- frontend/src/components/photos/Photo.vue
+++ frontend/src/components/photos/Photo.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>{{ object.title }}</div>
+  <Activity :object="object" :is-detail="isDetail">
+    <template v-slot:content>
+      <img
+        class="img-responsive"
+        :src="object.large_image_url"
+        width=""
+        height=""
+        loading="lazy"
+        :alt="object.title"
+      />
+    </template>
+  </Activity>
 </template>
 
 <script>
+import Activity from '@/components/activities/Activity';
+
 export default {
   name: 'Photo',
+  components: {
+    Activity,
+  },
   props: {
-    object: Object,
+    object: {
+      type: Object,
+      required: true,
+    },
+    isDetail: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
