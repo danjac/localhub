98318b6a57fd4265ac28e6e65383bf52eb395d1b
Dan Jacob <danjac2018@gmail.com>
2020-06-28 01:09:52 +0300

    Celery and mailhog fixes

---
+++ localhub/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2020 by Dan Jacob
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+# Localhub
+from localhub.config.celery_app import app as celery_app
+
+__all__ = ["celery_app"]
--- localhub/config/settings/heroku.py
+++ localhub/config/settings/heroku.py
@@ -3,14 +3,9 @@
 
 # Local
 from .base import *  # noqa
-from .base import env
 from .base.aws import *  # noqa
 from .base.mailgun import *  # noqa
 from .base.secure import *  # noqa
 
 # Required for Heroku SSL
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-BROKER_TRANSPORT_OPTIONS = {
-    "max_connections": env.int("MAX_BROKER_CONNECTIONS", default=2),
-}
