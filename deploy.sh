dcf116988727985719100c724ef5523db1a05213
Dan Jacob <danjac2018@gmail.com>
2020-10-08 18:03:52 +0300

    Add env example

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,10 @@
 set -e
 
 echo "Building assets..."
-docker-compose run --rm -e BASE_URL assets yarn build
+docker-compose run --rm assets yarn build
 
 echo "Deploying assets to S3..."
 docker-compose run --rm \
-    -e AWS_ACCESS_KEY_ID \
-    -e AWS_SECRET_ACCESS_KEY \
-    -e AWS_STORAGE_BUCKET_NAME \
     -e DJANGO_SETTINGS_MODULE=localhub.config.settings.deploy \
     django ./manage.py collectstatic --noinput
 
