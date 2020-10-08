638021ac714cc1575bf9bf71e110b2f7dc36833e
Dan Jacob <danjac2018@gmail.com>
2020-10-08 17:44:41 +0300

    Adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Building assets..."
+docker-compose run --rm -e BASE_URL assets yarn build
+
+echo "Deploying assets to S3..."
+docker-compose run --rm \
+    -e AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY \
+    -e AWS_STORAGE_BUCKET_NAME \
+    -e DJANGO_SETTINGS_MODULE=localhub.config.settings.deploy \
+    django ./manage.py collectstatic --noinput
+
+echo "Deploying to Heroku..."
+git push heroku master
+
+
+
