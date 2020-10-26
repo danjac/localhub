94f3e130150c9aaf3e8469ad801e7e1e381ea150
Dan Jacob <danjac2018@gmail.com>
2020-10-26 17:01:13 +0200

    Add help text for allow voting

--- localhub/activities/polls/forms.py
+++ localhub/activities/polls/forms.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2020 by Dan Jacob
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+# Django
+from django.utils.translation import gettext_lazy as _
+
 # Localhub
 from localhub.activities.forms import ActivityForm
 
@@ -13,3 +16,7 @@ class PollForm(ActivityForm):
         model = Poll
 
         fields = ActivityForm.Meta.fields + ("allow_voting",)
+        help_texts = {
+            **ActivityForm.Meta.help_texts,
+            "allow_voting": _("Voting is only allowed on public polls"),
+        }
