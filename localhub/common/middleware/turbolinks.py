11e83217b96291bab43d3bd916fac7b7e5f54771
Dan Jacob <danjac2018@gmail.com>
2020-11-11 19:35:59 +0200

    Add correct redirect header

--- localhub/common/middleware/turbolinks.py
+++ localhub/common/middleware/turbolinks.py
@@ -25,25 +25,24 @@ class TurbolinksMiddleware:
 
         response = self.get_response(request)
         if response.status_code in (301, 302) and response.has_header("Location"):
-            return self.handle_redirect(request, response)
+            return self.handle_redirect(request, response, response["Location"])
         if response.status_code in range(200, 299):
             location = request.session.pop(self.session_key, None)
             if location:
                 response[self.location_header] = location
         return response
 
-    def handle_redirect(self, request, response):
+    def handle_redirect(self, request, response, location):
         if self.is_turbolinks_request(request):
-            return self.redirect_with_turbolinks(request, response)
-        request.session[self.session_key] = response["Location"]
+            return self.redirect_with_turbolinks(request, response, location)
+        request.session[self.session_key] = location
         return response
 
     def is_turbolinks_request(self, request):
         return request.is_ajax() and self.referrer_header in request.headers
 
-    def redirect_with_turbolinks(self, request, response):
+    def redirect_with_turbolinks(self, request, response, location):
         js = []
-        location = response["Location"]
         if request.method not in ("GET", "HEAD", "OPTIONS", "TRACE"):
             js.append("Turbolinks.clearCache();")
         js.append(f"Turbolinks.visit('{location}');")
