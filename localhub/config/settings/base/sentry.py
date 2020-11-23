8642a73b532b120a7b38799995c0881a1554a4d2
Dan Jacob <danjac2018@gmail.com>
2020-11-24 01:09:53 +0200

    Add sentry support

---
+++ localhub/config/settings/base/sentry.py
@@ -0,0 +1,15 @@
+# Third Party Libraries
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+# Local
+from . import env
+
+sentry_sdk.init(
+    dsn=env("SENTRY_URL"),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
--- localhub/config/settings/heroku.py
+++ localhub/config/settings/heroku.py
@@ -6,6 +6,7 @@ from .base import *  # noqa
 from .base.aws import *  # noqa
 from .base.mailgun import *  # noqa
 from .base.secure import *  # noqa
+from .base.sentry import *  # noqa
 
 # Required for Heroku SSL
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
