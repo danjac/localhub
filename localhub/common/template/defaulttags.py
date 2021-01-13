9e7062df99298bef50fdc12af1ba998468e02ccf
Dan Jacob <danjac2018@gmail.com>
2021-01-13 13:27:13 +0200

    Active link tags

--- localhub/common/template/defaulttags.py
+++ localhub/common/template/defaulttags.py
@@ -1,13 +1,43 @@
 # Copyright (c) 2020 by Dan Jacob
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+# Standard Library
+import collections
+import re
+
 # Django
 from django import template
+from django.shortcuts import resolve_url
 
 # Localhub
 from localhub.common.utils import emojis
 
 register = template.Library()
+
+ActiveLink = collections.namedtuple("Link", "url match exact")
+
+
+@register.simple_tag(takes_context=True)
+def active_link(context, url_name, *args, **kwargs):
+    url = resolve_url(url_name, *args, **kwargs)
+    if context["request"].path == url:
+        return ActiveLink(url, True, True)
+    elif context["request"].path.startswith(url):
+        return ActiveLink(url, True, False)
+    return ActiveLink(url, False, False)
+
+
+@register.simple_tag(takes_context=True)
+def active_link_regex(context, pattern, url_name, *args, **kwargs):
+    url = resolve_url(url_name, *args, **kwargs)
+
+    if context["request"].path == url:
+        return ActiveLink(url, True, True)
+
+    if re.compile(pattern).match(context["request"].path):
+        return ActiveLink(url, True, False)
+
+    return ActiveLink(url, False, False)
 
 
 @register.simple_tag
--- localhub/common/template/tests/test_defaulttags.py
+++ localhub/common/template/tests/test_defaulttags.py
@@ -3,6 +3,67 @@
 
 # Django
 from django import template
+from django.urls import reverse
+
+# Third Party Libraries
+import pytest
+
+# Local
+from ..defaulttags import active_link, active_link_regex
+
+pytestmark = pytest.mark.django_db
+
+
+class TestActiveLink:
+    def test_no_match(self, rf):
+        url = reverse("account_login")
+        req = rf.get(url)
+        route = active_link({"request": req}, "posts:list")
+        assert route.url == reverse("posts:list")
+        assert not route.match
+        assert not route.exact
+
+    def test_non_exact_match(self, rf, post):
+        url = post.get_absolute_url()
+        req = rf.get(url)
+        route = active_link({"request": req}, "posts:list")
+        assert route.url == reverse("posts:list")
+        assert route.match
+        assert not route.exact
+
+    def test_exact_match(self, rf):
+        url = reverse("posts:list")
+        req = rf.get(url)
+        route = active_link({"request": req}, "posts:list")
+        assert route.url == reverse("posts:list")
+        assert route.match
+        assert route.exact
+
+
+class TestActiveLinkRegex:
+    def test_no_match(self, rf):
+        url = reverse("account_login")
+        req = rf.get(url)
+        route = active_link_regex({"request": req}, "/posts/", "posts:list")
+        assert route.url == reverse("posts:list")
+        assert not route.match
+        assert not route.exact
+
+    def test_non_exact_match(self, rf, post):
+        url = post.get_absolute_url()
+        req = rf.get(url)
+        route = active_link_regex({"request": req}, "/posts/", "posts:list")
+        assert route.url == reverse("posts:list")
+        assert route.match
+        assert not route.exact
+
+    def test_exact_match(self, rf):
+        url = reverse("posts:list")
+        req = rf.get(url)
+        route = active_link_regex({"request": req}, "/posts/", "posts:list")
+        assert route.url == reverse("posts:list")
+        assert route.match
+        assert route.exact
 
 
 class TestCollapsable:
